f5b8d94b84898317147264e69f042f520912678a
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2025-12-14 20:59:02 +0530

    Time: 19 ms (83.5%), Space: 33.6 MB (91.5%) - LeetHub

---
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.cpp
@@ -0,0 +1,22 @@
+class Solution {
+#define ll long long int
+const ll mod = 1000000007;
+public:
+    int numberOfWays(string s) {
+        ll n = s.size(), res = 1, seats = 0, plants = 0;
+
+        for (int i = 0, j = 0; i < n; i++) {
+            if (s[i] == 'S') {
+                if (++seats > 2 && seats % 2) {
+                    res *= (plants + 1);
+                    res %= mod;
+                }
+                plants = 0;
+            } else {
+                plants++;
+            }
+        }
+
+        return seats % 2 == 0 && seats > 0 ? res : 0;
+    }
+};
