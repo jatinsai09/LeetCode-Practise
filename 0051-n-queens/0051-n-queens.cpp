885d70ebe3cd5317b2cfeca75875ff85277b01b1
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2025-12-18 10:34:03 +0530

    Time: 0 ms (100%), Space: 11.1 MB (26.87%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> res;
+        vector<string> b(n, string(n, '.'));
+
+        vector<int> c(n), md(2 * n + 1), od(2 * n - 1);
+
+        function<void(int)> f = [&](int row) -> void {
+            if (row == n) {
+                res.push_back(b);
+            }
+
+            for (int col = 0; col < n; col++) {
+                if (!c[col] && !md[row + col] && !od[n - 1 + col - row]) {
+                    c[col] = 1;
+                    md[row + col] = 1;
+                    od[n - 1 + col - row] = 1;
+                    b[row][col] = 'Q';
+
+                    f(row + 1);
+
+                    c[col] = 0;
+                    md[row + col] = 0;
+                    od[n - 1 + col - row] = 0;
+                    b[row][col] = '.';                    
+                }
+            }
+        };
+
+        f(0);
+        return res;
+    }
+};
