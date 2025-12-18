2b11c00d8bafc06fd8072cb7e14fd151823ee3bd
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2025-12-18 09:15:06 +0530

    Time: 0 ms (100%), Space: 10.8 MB (39.84%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-    vector<vector<int>> permute(vector<int>& nums) {
-        int n = nums.size();
+    vector<vector<int>> permute(vector<int>& a) {
+        int n = a.size();
         vector<vector<int>> res;
 
-        function<void(int, vector<int>)> f = [&](int ind, auto a) {
+        function<void(int)> f = [&](int ind) -> void {
             if (ind == n) {
                 res.push_back(a);
                 return;
@@ -12,12 +12,12 @@ public:
 
             for (int i = ind; i < n; i++) {
                 swap(a[i], a[ind]);
-                f(ind + 1, a);
+                f(ind + 1);
+                swap(a[i], a[ind]);
             }
         };
 
-        f(0, nums);
-
+        f(0);
         return res;
     }
 };
