ea04808c769a87a5a3c4d228e0119f4276f7b960
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2025-12-18 14:52:05 +0530

    Time: 11 ms (23.81%), Space: 19.1 MB (83.1%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool canFinish(int num, vector<vector<int>>& pre) {
+        vector<vector<int>> adj(num);
+
+        for (const auto& p : pre) {
+            adj[p[0]].push_back(p[1]);
+        }
+
+        int indegree[num];
+        memset(indegree, 0, sizeof(indegree));
+        for (int u = 0; u < num; u++) {
+            for (const auto& v : adj[u]) {
+                indegree[v]++;
+            }
+        }
+
+        queue<int> q;
+        for (int v = 0; v < num; v++) {
+            if (indegree[v] == 0) {
+                q.push(v);
+            }
+        }
+
+        int c = 0;
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            c++;
+
+            for (const auto& v : adj[node]) {
+                if (--indegree[v] == 0) {
+                    q.push(v);
+                }
+            }
+        }
+        
+        return c == num;
+    }
+};
