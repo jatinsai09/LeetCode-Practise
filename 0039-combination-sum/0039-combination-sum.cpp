7146e6ef8aa0e16b4027b80a58ad386c2d47a156
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2025-12-18 09:33:20 +0530

    Time: 8 ms (24.55%), Space: 19.1 MB (20.04%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        int n = candidates.size();
+        vector<vector<int>> res;
+        vector<int> v;
+
+        function<void(int)> f = [&](int i) -> void {
+            if (target == 0) {
+                res.push_back(v);
+                return;
+            }
+
+            if (i == n || target < 0) {
+                return;
+            }
+
+            v.push_back(candidates[i]);
+            target -= candidates[i];
+            f(i);
+            
+            v.pop_back();
+            target += candidates[i];
+            f(i + 1);
+        };
+
+        f(0);
+        return res;
+    }
+};
