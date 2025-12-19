95d40a6c6053d5844ffb2ca059c5e2ff531acfc8
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2025-12-19 15:53:19 +0530

    Time: 1 ms (86.16%), Space: 13 MB (88.35%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -3,25 +3,34 @@ public:
     vector<string> generateParenthesis(int n) {
         vector<string> res;
         string s;
+        int open = n, close = n;
 
-        function<void(int, int)> f = [&](int open, int close) -> void {
+        function<void()> f = [&]() -> void {
             if (open + close == 0) {
                 res.push_back(s);
             }
 
             if (open) {
                 s.push_back('(');
-                f(open - 1, close);
+                open--;
+
+                f();
+
+                open++;
                 s.pop_back();
             }
             if (close > open) {
                 s.push_back(')');
-                f(open, close - 1);
+                close--;
+
+                f();
+
+                close++;
                 s.pop_back();
             }
         };
 
-        f(n, n);
+        f();
         return res;
     }
 };
