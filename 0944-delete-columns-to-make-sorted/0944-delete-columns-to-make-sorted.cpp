0303cb529fbfc0a2e421d2fa702ce9678a8d1861
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2025-12-20 09:02:42 +0530

    Time: 11 ms (9.35%), Space: 15.4 MB (77.57%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
@@ -5,7 +5,7 @@ public:
 
         for (int j = 0; j < m; j++) {
             int f = 0;
-            for (int i = 1; i < n; i++) {
+            for (int i = 1; i < n && !f; i++) {
                 f |= (strs[i][j] < strs[i - 1][j]);
             }
             res += f;
