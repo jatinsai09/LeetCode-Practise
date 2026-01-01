7914612b9d078797cfd19236ede87585b1c174e2
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-01-01 20:12:34 +0530

    Time: 3 ms (21.71%), Space: 12.5 MB (18.92%) - LeetHub

---
+++ 0840-magic-squares-in-grid/0840-magic-squares-in-grid.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int numMagicSquaresInside(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size(), cnt = 0;
+
+        auto isMagicSquare = [&](int r, int c) -> bool {
+            int sum = grid[r][c] + grid[r][c + 1] + grid[r][c + 2];
+            unordered_set<int> st;
+
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    int num = grid[r + i][c + j];
+                    if (num < 1 || num > 9 || st.count(num))
+                        return false;
+                    st.insert(num);
+                }
+            }
+
+            for (int k = 0; k < 3; k++) {
+                if (grid[r][c + k] + grid[r + 1][c + k] + grid[r + 2][c + k] !=
+                    sum)
+                    return false;
+                if (grid[r + k][c] + grid[r + k][c + 1] + grid[r + k][c + 2] !=
+                    sum)
+                    return false;
+            }
+
+            if (grid[r][c] + grid[r + 1][c + 1] + grid[r + 2][c + 2] != sum)
+                return false;
+            if (grid[r][c + 2] + grid[r + 1][c + 1] + grid[r + 2][c] != sum)
+                return false;
+
+            return true;
+        };
+
+        for (int r = 0; r + 2 < m; r++) {
+            for (int c = 0; c + 2 < n; c++) {
+                if (isMagicSquare(r, c))
+                    cnt++;
+            }
+        }
+        return cnt;
+    }
+};
