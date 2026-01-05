f14f1e30ace0700dae0df6e931521a1ac83c1c3d
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-01-05 14:06:53 +0530

    Time: 0 ms (100%), Space: 54.8 MB (99.86%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -7,17 +7,17 @@ public:
 
         int n = nums.size();
         for (int i = 0; i < n; i++) {
-            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+            while (nums[i] >= 1 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
                 swap(nums[i], nums[nums[i] - 1]);
             }
         }
 
-        for (int i = 0; i < n; i++) {
-            if (nums[i] != i + 1) {
-                return i + 1;
+        for (int i = 1; i <= n; i++) {
+            if (nums[i - 1] != i) {
+                return i;
             }
         }
-        
+
         return n + 1;
     }
 };
