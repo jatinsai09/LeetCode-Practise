070a7fb4429a71b6c650410b2a5dfffaf65683d9
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-01-06 18:46:44 +0530

    Time: 0 ms (100%), Space: 8.7 MB (15.51%) - LeetHub

--- 1104-path-in-zigzag-labelled-binary-tree/1104-path-in-zigzag-labelled-binary-tree.cpp
+++ 1104-path-in-zigzag-labelled-binary-tree/1104-path-in-zigzag-labelled-binary-tree.cpp
@@ -6,8 +6,8 @@ public:
         int h = log2(label);
 
         while(h) {
-            int pl = pow(2, h - 1), cr = pow(2, h + 1) - 1;
-            label = pl + (cr - label) / 2;
+            int prevLow = pow(2, h - 1), curHigh = pow(2, h + 1) - 1;
+            label = prevLow + (curHigh - label) / 2;
 
             res.push_back(label);
             h--;
