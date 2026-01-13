a761883f94d09236a7ed3665e8444f375ffaa6b0
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-01-13 13:29:53 +0530

    Time: 0 ms (100%), Space: 10.8 MB (92.26%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -6,24 +6,47 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
         vector<int> res;
-        postorder(root, res);
+        // postorder(root, res);
+        stack<TreeNode*> st;
+        TreeNode* cur = root;
+        TreeNode* last = NULL;
+
+        while ((cur) || (!st.empty())) {
+            if (cur) {
+                st.push(cur);
+                cur = cur->left;
+            } else {
+                TreeNode* node = st.top();
+
+                if (node->right && last != node->right) {
+                    cur = node->right;
+                } else {
+                    res.push_back(node->val);
+
+                    last = node;
+                    st.pop();
+                }
+            }
+        }
+
         return res;
     }
 
-    void postorder(TreeNode* root, vector<int>& res) {
-        if (root == nullptr) {
+    void postorder(TreeNode* node, vector<int>& res) {
+        if (node == NULL) {
             return;
         }
 
-        postorder(root->left, res);
-        postorder(root->right, res);
-        res.push_back(root->val);
+        postorder(node->left, res);
+        postorder(node->right, res);
+        res.push_back(node->val);
     }
 };
