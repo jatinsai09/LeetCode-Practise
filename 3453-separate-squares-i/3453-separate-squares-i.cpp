28166a6fbdc1af71be222a7e3e8566ed95a07afb
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-01-13 18:16:04 +0530

    Time: 143 ms (30.64%), Space: 198.8 MB (52.02%) - LeetHub

---
+++ 3453-separate-squares-i/3453-separate-squares-i.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    double separateSquares(vector<vector<int>>& squares) {
+        int n = squares.size();
+        double lo = 0, hi = 2 * 1e9;
+
+        auto f = [&](double line) -> double {
+            double aAbove = 0, aBelow = 0;
+            for (int i = 0; i < n; i++) {
+                int x = squares[i][0], y = squares[i][1];
+                int l = squares[i][2];
+                double total = (double)l * l;
+
+                if (line <= y) {
+                    aAbove += total;
+                } else if (line >= y + l) {
+                    aBelow += total;
+                } else {
+                    double aboveHeight = (y + l) - line;
+                    double belowHeight = line - y;
+                    aAbove += l * aboveHeight;
+                    aBelow += l * belowHeight;
+                }
+            }
+            return aAbove - aBelow;
+        };
+
+        for (int i = 0; i < 60; i++) {
+            double mid = (lo + hi) / 2.0;
+            double diff = f(mid);
+
+            if (diff > 0) {
+                lo = mid;
+            } else {
+                hi = mid;
+            }
+        }
+
+        return hi;
+    }
+};
