1221ba6fc9f9bc6264ec4fb706bc054dddf5c0bd
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-01-16 10:14:31 +0530

    Time: 1311 ms (55.34%), Space: 267 MB (83.5%) - LeetHub

--- 2975-maximum-square-area-by-removing-fences-from-a-field/2975-maximum-square-area-by-removing-fences-from-a-field.cpp
+++ 2975-maximum-square-area-by-removing-fences-from-a-field/2975-maximum-square-area-by-removing-fences-from-a-field.cpp
@@ -9,9 +9,6 @@ public:
         hFences.push_back(1);
         hFences.push_back(m);
 
-        vFences.push_back(1);
-        vFences.push_back(n);
-
         for (int i = 0; i < hFences.size(); i++) {
             for (int j = 0; j < hFences.size(); j++) {
                 if (i == j) {
@@ -20,6 +17,9 @@ public:
                 st.insert(abs(hFences[i] - hFences[j]));
             }
         }
+
+        vFences.push_back(1);
+        vFences.push_back(n);
 
         for (int i = 0; i < vFences.size(); i++) {
             for (int j = 0; j < vFences.size(); j++) {
