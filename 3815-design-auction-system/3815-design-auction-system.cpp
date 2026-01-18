cf7afc361549db1e462799bc40c3837231306b96
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-01-18 14:33:38 +0530

    Time: 197 ms (48.78%), Space: 362 MB (56.38%) - LeetHub

--- 3815-design-auction-system/3815-design-auction-system.cpp
+++ 3815-design-auction-system/3815-design-auction-system.cpp
@@ -7,35 +7,35 @@ public:
     }
 
     void addBid(int userId, int itemId, int bidAmount) {
-        if(mp[itemId].count(userId)) {
-            int old = mp[itemId][userId];
-            st[itemId].erase({old, userId});
+        if(mp[itemId].count(userId)) { // if this user already holds a bid for this item
+            int old = mp[itemId][userId]; 
+            st[itemId].erase({old, userId}); // remove old bid
         }
-        mp[itemId][userId] = bidAmount;
-        st[itemId].insert({bidAmount, userId});
+        mp[itemId][userId] = bidAmount; // update bid amount
+        st[itemId].insert({bidAmount, userId}); // add new bid
     }
 
     void updateBid(int userId, int itemId, int newAmount) {
         int old = mp[itemId][userId];
-        st[itemId].erase({old, userId});
-        mp[itemId][userId] = newAmount;
-        st[itemId].insert({newAmount, userId});
+        st[itemId].erase({old, userId}); // remove old bid
+        mp[itemId][userId] = newAmount; // update bid amount
+        st[itemId].insert({newAmount, userId}); // add new bid
     }
 
     void removeBid(int userId, int itemId) {
         int old = mp[itemId][userId];
-        st[itemId].erase({old, userId});
-        mp[itemId].erase(userId);
-        if(mp[itemId].empty()) {
+        st[itemId].erase({old, userId}); // remove current bid
+        mp[itemId].erase(userId); // remove this bidder for the item
+        if(mp[itemId].empty()) { // if the item has no acitve bid left
             mp.erase(itemId);
-            st.erase(itemId);
+            st.erase(itemId); // remove
         }
     }
 
     int getHighestBidder(int itemId) {
-        if(!st.count(itemId) || st[itemId].empty()) {
+        if(!st.count(itemId) || st[itemId].empty()) { // if this item has no active bids presnt
             return -1;
         }
-        return st[itemId].rbegin()->second;
+        return st[itemId].rbegin()->second; // return user who holds the highest bid for this item
     }
 };
