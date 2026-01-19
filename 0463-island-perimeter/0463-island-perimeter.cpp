18d3bb0356e91bd5fe1f8b7defa26fd2e71f250e
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-01-19 15:42:07 +0530

    Time: 0 ms (100%), Space: 101.9 MB (85.13%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -6,17 +6,12 @@ public:
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (grid[i][j] == 1) {
-                    if (i <= 0 || grid[i - 1][j] != 1) {
-                        perimeter++;
+                    perimeter += 4;
+                    if (i > 0 && grid[i - 1][j] == 1) {
+                        perimeter -= 2;
                     }
-                    if (j <= 0 || grid[i][j - 1] != 1) {
-                        perimeter++;
-                    }
-                    if (i + 1 >= n || grid[i + 1][j] != 1) {
-                        perimeter++;
-                    }
-                    if (j + 1 >= m || grid[i][j + 1] != 1) {
-                        perimeter++;
+                    if (j > 0 && grid[i][j - 1] == 1) {
+                        perimeter -= 2;
                     }
                 } 
             }
