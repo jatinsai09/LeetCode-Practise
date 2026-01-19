a239d32825b7f9c3db125b9b4eed67a7033ed970
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-01-19 15:47:47 +0530

    Time: 0 ms (100%), Space: 16.9 MB (52.65%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+
+        vector<int> dir = {-1, 0, 1, 0, -1};
+
+        int r = grid.size(), c = grid[0].size();
+        int vis[r][c];
+
+        queue<pair<pair<int, int>, int>> q;
+        int fresh = 0;
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (grid[i][j] == 2) {
+                    vis[i][j] = 2;
+                    q.push({{i, j}, 0});
+                } else if (grid[i][j] == 1) {
+                    fresh++;
+                    vis[i][j] = 1;
+                } else {
+                    vis[i][j] = 0;
+                }
+            }
+        }
+
+        int maxt = 0, c1 = 0;
+        while (!q.empty()) {
+            int sz = q.size();
+            pair<pair<int, int>, int> p = q.front();
+            int t = p.second;
+            maxt = max(t, maxt);
+            q.pop();
+            for (int i = 0; i < 4; i++) {
+                int dr = p.first.first + dir[i];
+                int dc = p.first.second + dir[i + 1];
+
+                if (dr >= 0 && dr < r && dc >= 0 && dc < c &&
+                    vis[dr][dc] == 1) {
+                    vis[dr][dc] = 2;
+                    q.push({{dr, dc}, t + 1});
+                    c1++;
+                }
+            }
+            t++;
+        }
+        if (fresh != c1) {
+            return -1;
+        }
+
+        return maxt;
+    }
+};
