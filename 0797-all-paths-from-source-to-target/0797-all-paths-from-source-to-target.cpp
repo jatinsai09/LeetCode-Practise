e2bb089cfb7fbad485e480aa6158e788913140d0
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-01-19 10:10:38 +0530

    Time: 7 ms (45.5%), Space: 15 MB (57.45%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -3,19 +3,21 @@ public:
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
         int n = graph.size();
         vector<vector<int>> res;
-        vector<int> path = {0};
+        vector<int> path;
 
         function<void(int)> dfs = [&](int u) -> void {
             if (u == n - 1) {
+                path.push_back(u);
                 res.push_back(path);
+                path.pop_back();
                 return;
             }
 
+            path.push_back(u);
             for (auto& v : graph[u]) {
-                path.push_back(v);
                 dfs(v);
-                path.pop_back();
             }
+            path.pop_back();
         };
 
         dfs(0);
