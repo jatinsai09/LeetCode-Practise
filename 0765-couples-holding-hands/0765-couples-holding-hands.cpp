fe337393897bcfd8e067efd9f989a6ed41a15181
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-01-19 13:25:55 +0530

    Time: 0 ms (100%), Space: 9.9 MB (75.92%) - LeetHub

--- 0765-couples-holding-hands/0765-couples-holding-hands.cpp
+++ 0765-couples-holding-hands/0765-couples-holding-hands.cpp
@@ -4,19 +4,25 @@ public:
         int n = row.size(), swaps = 0;
         vector<int> pos(n);
 
+        // Map each person to their seat number (position)
         for (int i = 0; i < n; i++) {
             pos[row[i]] = i;
         }
 
+        // Process each pair of seats
         for (int i = 0; i < n; i += 2) {
-            int f = row[i], got = row[i + 1], req = f ^ 1;
+            int f = row[i]; // first person
+            int got = row[i + 1]; // Present partner of first person (second person)
+            int req = f ^ 1; // Required partner of first person 
 
             if (req != got) {
                 swaps++;
                 int partnerIndex = pos[req];
 
+                // Swap second person with the required partner
                 swap(row[i + 1], row[partnerIndex]);
 
+                // Update the pos
                 pos[req] = i + 1;
                 pos[got] = partnerIndex;
             }
