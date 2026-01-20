9f805097f04dbe54395148a3c6cb20b97c5473fd
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-01-20 14:46:13 +0530

    Time: 0 ms (100%), Space: 10.1 MB (77.89%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -4,8 +4,7 @@ public:
         int n = nums.size(), prev = nums[0], prev2 = 0, cur = nums[0];
 
         for (int i = 1; i < n; i++) {
-            int pick = nums[i] + prev2;
-            int not_pick = 0 + prev;
+            int pick = nums[i] + prev2, not_pick = prev;
 
             cur = max(pick, not_pick);
             prev2 = prev;
