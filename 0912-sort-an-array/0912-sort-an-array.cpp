b9b0cd078e1d3529dcc60ea0d9838cd0ac364ce0
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-01-20 11:52:06 +0530

    Time: 413 ms (40.62%), Space: 71 MB (83.68%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void quick_sort(auto& nums, int l, int r) {
+        if (l >= r) {
+            return;
+        }
+        int pivot = nums[l + (r - l) / 2], i = l, j = r;
+        while (i <= j) {
+            while (nums[i] < pivot) {
+                i++;
+            }
+            while (nums[j] > pivot) {
+                j--;
+            }
+            if (i <= j) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+                j--;
+            }
+        }
+        quick_sort(nums, l, j);
+        quick_sort(nums, i, r);
+    }
+    vector<int> sortArray(vector<int>& nums) {
+        int n = nums.size();
+        quick_sort(nums, 0, n - 1);
+
+        return nums;
+    }
+};
