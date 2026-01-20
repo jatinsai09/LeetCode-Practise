90fb61d547124d923ff14a2e2a7fd96f804ed3be
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-01-20 12:20:46 +0530

    Time: 0 ms (100%), Space: 15.6 MB (78.83%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -8,9 +8,9 @@ public:
         for (int j = 1; j < m; j++) {
             prev[j] = grid[0][j] + prev[j - 1];
         }
-        
+
         for (int i = 1; i < n; i++) {
-            cur[0] = prev[0] + grid[i][0];
+            cur[0] = grid[i][0] + prev[0];
             for (int j = 1; j < m; j++) {
                 int up = grid[i][j] + prev[j];
                 int left = grid[i][j] + cur[j - 1];
