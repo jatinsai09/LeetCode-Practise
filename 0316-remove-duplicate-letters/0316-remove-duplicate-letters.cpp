202e015e73f72a2a99d191e3c1fde37e138cee3a
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-01-21 12:24:42 +0530

    Time: 0 ms (100%), Space: 9.2 MB (83.42%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        int n = s.size();
+        vector<int> f(26), vis(26);
+        for (auto &i: s) {
+            f[i - 'a']++;
+        }
+
+        string res = "";
+        for (auto &i: s) {
+            f[i - 'a']--;
+            if (vis[i - 'a']) {
+                continue;
+            }
+
+            vis[i - 'a'] = 1;
+            while (!res.empty() && res.back() > i && f[res.back() - 'a']) {
+                vis[res.back() - 'a'] = 0;
+                res.pop_back();
+            }
+            res.push_back(i);
+        }
+
+        return res;
+    }
+};
