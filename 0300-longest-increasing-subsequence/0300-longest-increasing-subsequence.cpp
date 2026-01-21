809e4a781c48c1e49152fea7571694f2c9aff0c2
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-01-21 09:11:02 +0530

    Time: 0 ms (100%), Space: 14.1 MB (88.25%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+
+        vector<int> v;
+        v.push_back(nums[0]);
+        int n = nums.size(), len = 1;
+
+        for (int i = 1; i < n; i++) {
+            if (v.back() < nums[i]) {
+                v.push_back(nums[i]);
+                len++;
+            } else {
+                int ind = lower_bound(v.begin(), v.end(), nums[i]) - v.begin();
+                v[ind] = nums[i];
+            }
+        }
+        return len;
+    }
+};
