90719715a94592231760f9fc3c233eebb141a153
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-01-23 10:14:05 +0530

    Time: 0 ms (100%), Space: 8.7 MB (93.99%) - LeetHub

--- 3407-substring-matching-pattern/3407-substring-matching-pattern.cpp
+++ 3407-substring-matching-pattern/3407-substring-matching-pattern.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    bool hasMatch(string s, string p) {
-        int x = p.find("*");
+    bool hasMatch(string& s, string& p) {
+        const int x = p.find("*");
         string a = p.substr(0, x);
         string b = p.substr(x + 1);
 
-        int i = s.find(a), j = s.rfind(b);
+        const int i = s.find(a), j = s.rfind(b);
 
         return i != -1 && j != -1 && i + a.size() <= j;
     }
