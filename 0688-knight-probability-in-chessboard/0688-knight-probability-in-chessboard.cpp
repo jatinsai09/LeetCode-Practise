448f0a8102db7930b04999f784a2320b507dc50e
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-01-30 10:02:32 +0530

    Time: 514 ms (5.28%), Space: 21 MB (19.86%) - LeetHub

--- 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.cpp
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.cpp
@@ -3,7 +3,7 @@ public:
     double knightProbability(int n, int k, int row, int column) {
         vector<int> dr = {-2, -2, 2, 2, -1, -1, 1, 1};
         vector<int> dc = {-1, 1, -1, 1, -2, 2, -2, 2};
-        unordered_map<string, double> dp;
+        map<string, double> dp;
 
         function<double(int, int, int)> f = [&](int r, int c, int k) -> double {
             if (r < 0 || c < 0 || r >= n || c >= n) {
