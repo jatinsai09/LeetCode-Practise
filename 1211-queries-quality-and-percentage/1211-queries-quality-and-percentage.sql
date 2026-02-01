8ac1c33c63bc82b4ad4c8f349e219a10473dd865
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-02-01 20:38:58 +0530

    Time: 364 ms (36.76%), Space: 0B (100%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -6,7 +6,7 @@ SELECT
         2
     ) AS quality,
     ROUND(
-        (SUM(rating < 3) / COUNT(*)) * 100,
+        SUM(rating < 3) / COUNT(*) * 100,
         2
     ) AS poor_query_percentage
 FROM Queries
