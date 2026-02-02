357af27e061f643af5fce921d516c10e9df1ddb3
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-02-02 22:14:10 +0530

    Time: 0 ms (100%), Space: 36.4 MB (98.41%) - LeetHub

---
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool findTarget(TreeNode* root, int k) { return dfs(root, root, k); }
+
+    bool dfs(TreeNode* root, TreeNode* cur, int k) {
+        if (!cur) {
+            return false;
+        }
+        return find(root, cur, k - cur->val) || dfs(root, cur->left, k) ||
+               dfs(root, cur->right, k);
+    }
+
+    bool find(TreeNode* root, TreeNode* cur, int val) {
+        if (!root) {
+            return false;
+        }
+
+        if (root->val == val && root != cur) {
+            return true;
+        }
+
+        return find(root->left, cur, val) || find(root->right, cur, val);
+    }
+};
