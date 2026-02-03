2b0721f266390a8a2593d308ec4437c5f5533bae
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-02-03 07:49:28 +0530

    Time: 0 ms (100%), Space: 27.8 MB (35.38%) - LeetHub

---
+++ 3637-trionic-array-i/3637-trionic-array-i.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isTrionic(vector<int>& a) {
+        if (a[1] <= a[0]) {
+            return 0;
+        }
+        int n = a.size();
+        int slope = 1, c = 1;
+        for (int i = 1; i < n; i++) {
+            if (a[i] == a[i - 1]) {
+                return 0;
+            }
+
+            if (slope) {
+                if (a[i] < a[i - 1]) {
+                    slope = 1 - slope;
+                    c++;
+                }
+            } else {
+                if (a[i] > a[i - 1]) {
+                    slope = 1 - slope;
+                    c++;
+                }
+            }
+        }
+        return c == 3;
+    }
+};
