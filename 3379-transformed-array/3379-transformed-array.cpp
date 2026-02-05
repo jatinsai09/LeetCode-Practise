43bb078415a7297119427b294a3c807ee94c95d4
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-02-05 07:53:29 +0530

    Time: 4 ms (79.67%), Space: 25.7 MB (70.75%) - LeetHub

--- 3379-transformed-array/3379-transformed-array.cpp
+++ 3379-transformed-array/3379-transformed-array.cpp
@@ -5,7 +5,10 @@ public:
         vector<int> res(n);
 
         for (int i = 0; i < n; i++) {
-            int di = (i + (nums[i] % n) + n) % n;
+            int di = (i + nums[i]) % n;
+            if (di < 0) {
+                di += n;
+            }
             res[i] = nums[di]; 
         }
 
