422794066f0e9b40f658947f337158b513c80a4b
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-02-05 07:52:13 +0530

    Time: 8 ms (46.52%), Space: 25.6 MB (91.36%) - LeetHub

---
+++ 3379-transformed-array/3379-transformed-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> constructTransformedArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> res(n);
+
+        for (int i = 0; i < n; i++) {
+            int di = (i + (nums[i] % n) + n) % n;
+            res[i] = nums[di]; 
+        }
+
+        return res;
+    }
+};
