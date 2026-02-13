a267e2fb25b8d49f619bed26bb3a33dc4878c7ed
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-02-13 17:16:05 +0530

    Time: 1191 ms (73.93%), Space: 336.6 MB (37.6%) - LeetHub

--- 3714-longest-balanced-substring-ii/3714-longest-balanced-substring-ii.cpp
+++ 3714-longest-balanced-substring-ii/3714-longest-balanced-substring-ii.cpp
@@ -2,33 +2,30 @@ class Solution {
 public:
     int longestBalanced(string s) {
         int n = s.size(), res = 0;
-        
-        vector<int> f(3, 0);
+
         map<pair<int, int>, int> vis;
         vis[{0, 0}] = -1;
-        int p = -1, c = 0;
-        
+        int a = 0, b = 0, cnt = 0;
+
         for (int i = 0; i < n; i++) {
-            f[s[i] - 'a']++;
-            int np = s[i] - 'a';
-            if (p != np) {
-                c = 1;
-                p = np;
-            } else {
-                c++;
+            cnt = (i == 0 || s[i] == s[i - 1] ? cnt + 1 : 1);
+            if (s[i] == 'a') {
+                a++;
+            }
+            else if (s[i] == 'b') {
+                b++;
+            }
+            else {
+                a--;
+                b--;
             }
 
-            int dab = f[0] - f[1];
-            int dac = f[0] - f[2];
-            int dbc = f[1] - f[2];
-
-            if (vis.find({dab, dac}) != vis.end()) {
-                res = max(res, i - vis[{dab, dac}]);
+            res = max(res, cnt);
+            if (vis.count({a, b})) {
+                res = max(res, i - vis[{a, b}]);
             } else {
-                vis[{dab, dac}] = i;
+                vis[{a, b}] = i;
             }
-
-            res = max(res, c);
         }
 
         auto give = [&](char x, char y) -> void {
@@ -36,21 +33,17 @@ public:
             int d = 0;
             mp[0] = -1;
 
-            for(int i = 0; i < n; i++) {
-                if(s[i] != x && s[i] != y) {
+            for (int i = 0; i < n; i++) {
+                if (s[i] != x && s[i] != y) {
                     d = 0;
                     mp.clear();
                     mp[0] = i;
                     continue;
                 }
 
-                if(s[i] == x) {
-                    d++;
-                } else {
-                    d--;
-                }
+                d = (s[i] == x ? d + 1 : d - 1);
 
-                if(mp.count(d)) {
+                if (mp.count(d)) {
                     res = max(res, i - mp[d]);
                 } else {
                     mp[d] = i;
@@ -62,7 +55,6 @@ public:
         give('b', 'c');
         give('a', 'c');
 
-        
         return res;
     }
 };
