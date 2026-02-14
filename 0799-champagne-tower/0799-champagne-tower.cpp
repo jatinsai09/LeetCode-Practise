4e6065ec7ec5351ef469ce04082695149a97617c
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-02-14 08:19:57 +0530

    Time: 0 ms (100%), Space: 17.7 MB (52.06%) - LeetHub

--- 0799-champagne-tower/0799-champagne-tower.cpp
+++ 0799-champagne-tower/0799-champagne-tower.cpp
@@ -3,7 +3,7 @@ public:
     double champagneTower(int poured, int query_row, int query_glass) {
         vector<double> cur(1, poured);
 
-        for (int i = 1; i - 1 <= query_row; i++) {
+        for (int i = 1; i <= query_row + 1; i++) {
             vector<double> next(i + 1);
             for (int j = 0; j < i; j++) {
                 if (cur[j] >= 1) {
