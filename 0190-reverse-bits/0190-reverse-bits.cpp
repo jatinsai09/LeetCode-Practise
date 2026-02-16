1a43a9ca9b39318e2ccca654e23df84727a46f8a
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-02-16 07:20:57 +0530

    Time: 4 ms (29.28%), Space: 8.2 MB (73.18%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -4,7 +4,7 @@ public:
         uint32_t res = 0;
         for (int i = 0; i < 32; i++) {
             res <<= 1;
-            res |= (n & 1);
+            res ^= (n & 1);
             n >>= 1;
         }
         return res;
