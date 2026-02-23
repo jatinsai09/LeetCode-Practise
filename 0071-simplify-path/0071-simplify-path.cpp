1ce74debdc370ec105722cdc56fdfb4082b36b50
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-02-23 11:19:21 +0530

    Time: 4 ms (45.58%), Space: 11.1 MB (65.43%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        vector<string> st;
+        stringstream ss(path);
+        string t;
+
+        while (getline(ss, t, '/')) {
+            if (t == "" || t == ".") {
+                continue;
+            }
+
+            if (t == "..") {
+                if (!st.empty()) {
+                    st.pop_back();
+                }
+            } else {
+                st.push_back(t);
+            }
+        }
+
+        string res = "";
+        for (const auto& s: st) {
+            res += "/";
+            res += s;
+        }
+
+        return st.empty() ? "/" : res;
+    }
+};
