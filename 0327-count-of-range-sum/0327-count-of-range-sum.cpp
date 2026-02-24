5cef9db9df77b97b33910656c93571b71e5fdb31
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-02-24 11:29:29 +0530

    Time: 820 ms (23.76%), Space: 467.1 MB (5.99%) - LeetHub

--- 0327-count-of-range-sum/0327-count-of-range-sum.cpp
+++ 0327-count-of-range-sum/0327-count-of-range-sum.cpp
@@ -1,27 +1,48 @@
+
 class Solution {
 public:
     int countRangeSum(vector<int>& nums, int lower, int upper) {
         int n = nums.size();
+        vector<long long> prefix(n + 1, 0);
 
-        vector<long long> psum(n + 1, 0);
-        for (int i = 0; i < n; i++) {
-            psum[i + 1] = psum[i] + nums[i];
+        for (int i = 0; i < n; ++i)
+            prefix[i + 1] = prefix[i] + nums[i];
+
+        return mergeSort(prefix, 0, n + 1, lower, upper);
+    }
+
+private:
+    int mergeSort(vector<long long>& prefix, int left, int right, int lower, int upper) {
+        if (right - left <= 1) return 0;
+
+        int mid = left + (right - left) / 2;
+        int count = mergeSort(prefix, left, mid, lower, upper) +
+                    mergeSort(prefix, mid, right, lower, upper);
+
+        int j = mid, k = mid;
+        for (int i = left; i < mid; ++i) {
+            while (k < right && prefix[k] - prefix[i] < lower) ++k;
+            while (j < right && prefix[j] - prefix[i] <= upper) ++j;
+            count += j - k;
         }
 
-        multiset<long long> s;
-        long long res = 0;
-        for (int i = n; i >= 0; --i) {
-            long long low = psum[i] + lower;
-            long long high = psum[i] + upper;
+        // Merge step
+        vector<long long> temp;
+        int p1 = left, p2 = mid;
 
-            auto itLow = s.lower_bound(low);
-            auto itHigh = s.upper_bound(high);
-
-            res += distance(itLow, itHigh);
-
-            s.insert(psum[i]);
+        while (p1 < mid && p2 < right) {
+            if (prefix[p1] < prefix[p2])
+                temp.push_back(prefix[p1++]);
+            else
+                temp.push_back(prefix[p2++]);
         }
 
-        return res;
+        while (p1 < mid) temp.push_back(prefix[p1++]);
+        while (p2 < right) temp.push_back(prefix[p2++]);
+
+        for (int i = left; i < right; ++i)
+            prefix[i] = temp[i - left];
+
+        return count;
     }
 };
