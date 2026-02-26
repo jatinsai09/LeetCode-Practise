e0ba6b1f1c7b5ff64e52e828755523968b4b16b3
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-02-26 11:00:33 +0530

    Time: 10 ms (98.27%), Space: 60.9 MB (67.47%) - LeetHub

---
+++ 2560-house-robber-iv/2560-house-robber-iv.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minCapability(vector<int>& nums, int k) {
+        int mn = nums[0], mx = nums[0];
+        for (auto &i: nums) {
+            mn = min(mn, i);
+            mx = max(mx, i);
+        }
+
+        int n = nums.size(), l = mn, r = mx, mid;
+        while (l <= r) {
+            mid = l + (r - l) / 2;
+            int got = 0;
+            for (int i = 0; i < n; i++) {
+                if (nums[i] <= mid) {
+                    got++;
+                    i++;
+                }
+            }
+            if (got >= k) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+};
