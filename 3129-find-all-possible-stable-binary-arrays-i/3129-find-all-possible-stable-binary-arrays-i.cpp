4324613abdf9256c3d05a065722f859d64b98201
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-03-09 16:06:13 +0530

    Time: 393 ms (29.27%), Space: 51.2 MB (19.51%) - LeetHub

---
+++ 3129-find-all-possible-stable-binary-arrays-i/3129-find-all-possible-stable-binary-arrays-i.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    const int mod = 1e9 + 7;
+    int numberOfStableArrays(int zero, int one, int limit) {
+        vector<vector<vector<long long>>> dp(
+            one + 1, vector<vector<long long>>(zero + 1, vector<long long>(2)));
+        dp[0][0][0] = dp[0][0][1] = 1;
+
+        for (int i = 0; i <= one; i++) {
+            for (int j = 0; j <= zero; j++) {
+                for (int k = 1; k <= limit; k++) {
+                    if (i - k >= 0) {
+                        dp[i][j][1] = (dp[i][j][1] + dp[i - k][j][0]) % mod;
+                    }
+                    if (j - k >= 0) {
+                        dp[i][j][0] = (dp[i][j][0] + dp[i][j - k][1]) % mod;
+                    }
+                }
+            }
+        }
+        return (dp[one][zero][0] + dp[one][zero][1]) % mod;
+    }
+};
