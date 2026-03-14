e70710305497e51dec97422e065f9b3664d3f585
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-03-14 23:28:21 +0530

    Time: 163 ms (83.3%), Space: 294 MB (50.95%) - LeetHub

--- 3868-minimum-cost-to-equalize-arrays-using-swaps/3868-minimum-cost-to-equalize-arrays-using-swaps.cpp
+++ 3868-minimum-cost-to-equalize-arrays-using-swaps/3868-minimum-cost-to-equalize-arrays-using-swaps.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
     int minCost(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> f1(1e5), f2(1e5);
+        vector<int> f(1e5);
         for (auto &i: nums1) {
-            f1[i]++;
+            f[i]++;
         } 
         for (auto &i: nums2) {
-            f2[i]++;
+            f[i]--;
         }
 
         for (auto &i: nums1) {
-            if ((f1[i] + f2[i]) % 2) {
+            if (f[i] % 2) {
                 return -1;
             }
         }
 
         for (auto &i: nums2) {
-            if ((f1[i] + f2[i]) % 2) {
+            if (f[i] & 1) {
                 return -1;
             }
         }
 
-        int c1 = 0, c2 = 0;
+        int res = 0;
         for (int i = 0; i < 1e5; i++) {
-            if (f1[i] > f2[i]) {
-                c1 += (f1[i] - f2[i]) / 2;
-            } else {
-                c2 += (f2[i] - f1[i]) / 2;
+            if (f[i] > 0) {
+                res += (f[i] >> 1);
             }
         }
 
-        return c1 + c2 - min(c1, c2);
+        return res;
     }
 };
