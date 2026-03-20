fbb7e4a2f4bd76e07cdbc6e86691cc342cd28bbf
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-03-20 07:55:27 +0530

    Time: 10 ms (87.41%), Space: 30.1 MB (94.07%) - LeetHub

---
+++ 3567-minimum-absolute-difference-in-sliding-submatrix/3567-minimum-absolute-difference-in-sliding-submatrix.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<vector<int>> minAbsDiff(vector<vector<int>>& grid, int k) {
+        int m = grid.size(), n = grid[0].size();
+
+        int rows = m - k + 1, cols = n - k + 1;
+        vector<vector<int>> res(rows, vector<int>(cols, 0));
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                vector<int> val(k * k);
+                int ind = 0;
+                for (int x = i; x < i + k; x++) {
+                    for (int y = j; y < j + k; y++) {
+                        val[ind++] = grid[x][y];
+                    }
+                }
+                sort(begin(val), end(val));
+
+                int mn = INT_MAX;
+                for (int t = 1; t < val.size(); t++) {
+                    if (val[t] != val[t - 1]) {
+                        int del = val[t] - val[t - 1];
+                        if (del < mn) {
+                            mn = del;
+                        }
+                        if (mn == 0) {
+                            break;
+                        }
+                    }
+                }
+                res[i][j] = (mn == INT_MAX ? 0 : mn);
+            }
+        }
+        return res;
+    }
+};
