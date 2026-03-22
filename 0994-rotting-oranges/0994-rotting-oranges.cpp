1ac86e11abe756c83aacbc4a9c8ddf47a39ea583
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-03-22 09:30:20 +0530

    Time: 4 ms (15.53%), Space: 16.7 MB (71.33%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -5,50 +5,47 @@ public:
         cin.tie(0);
         cout.tie(0);
 
-        vector<int> dir = {0, -1, 0, 1, 0};
+        vector<int> dir = {-1, 0, 1, 0, -1};
 
         int r = grid.size(), c = grid[0].size();
-        vector<int> vis(r * c);
- 
-        queue<pair<int, int>> q;
+        int vis[r][c];
+
+        queue<pair<pair<int, int>, int>> q;
         int fresh = 0;
         for (int i = 0; i < r; i++) {
             for (int j = 0; j < c; j++) {
-                int key = i * c + j;
                 if (grid[i][j] == 2) {
-                    vis[key] = 2;
-                    q.push({key, 0});
+                    vis[i][j] = 2;
+                    q.push({{i, j}, 0});
                 } else if (grid[i][j] == 1) {
                     fresh++;
-                    vis[key] = 1;
+                    vis[i][j] = 1;
                 } else {
-                    vis[key] = 0;
+                    vis[i][j] = 0;
                 }
             }
         }
 
-        int maxt = 0, rotten = 0;
+        int maxt = 0, c1 = 0;
         while (!q.empty()) {
-            pair<int, int> p = q.front();
+            pair<pair<int, int>, int> p = q.front();
             q.pop();
-            int t = p.second, key = p.first;
-            int ri = key / c, ci = key % c;
+
+            int t = p.second;
             maxt = max(t, maxt);
 
             for (int i = 0; i < 4; i++) {
-                int dr = ri + dir[i];
-                int dc = ci + dir[i + 1];
-                int nkey = dr * c + dc;
+                int dr = p.first.first + dir[i];
+                int dc = p.first.second + dir[i + 1];
 
-                if (dr >= 0 && dr < r && dc >= 0 && dc < c &&
-                    vis[nkey] == 1) {
-                    vis[nkey] = 2;
-                    q.push({nkey, t + 1});
-                    rotten++;
+                if (dr >= 0 && dr < r && dc >= 0 && dc < c && vis[dr][dc] == 1) {
+                    vis[dr][dc] = 2;
+                    q.push({{dr, dc}, t + 1});
+                    c1++;
                 }
             }
         }
 
-        return (fresh == rotten ? maxt : -1);
+        return (fresh == c1 ? maxt : -1);
     }
 };
