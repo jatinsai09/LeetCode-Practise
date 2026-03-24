ba005b43eeec3eeed8a015135e950cf94eaf34b2
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-03-24 07:20:23 +0530

    Time: 19 ms (98.52%), Space: 131.9 MB (89.3%) - LeetHub

--- 2906-construct-product-matrix/2906-construct-product-matrix.cpp
+++ 2906-construct-product-matrix/2906-construct-product-matrix.cpp
@@ -3,34 +3,28 @@ public:
     vector<vector<int>> constructProductMatrix(vector<vector<int>>& grid) {
         const int mod = 12345;
         int n = grid.size(), m = grid[0].size();
-        vector<vector<int>> pre(n, vector<int>(m)), suf(pre), res(pre);
+        vector<vector<int>> res(n, vector<int>(m));
 
-        long long p = 1;
+        long long pre = 1;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                pre[i][j] = p;
-                p *= grid[i][j];
-                p %= mod;
+                res[i][j] = pre;
+
+                pre *= grid[i][j];
+                pre %= mod;
             }
         }
 
-        p = 1;
+        long long suf = 1;
         for (int i = n - 1; i >= 0; i--) {
             for (int j = m - 1; j >= 0; j--) {
-                suf[i][j] = p;
-                p *= grid[i][j];
-                p %= mod;
+                res[i][j] = (res[i][j] * suf) % mod;
+
+                suf *= grid[i][j];
+                suf %= mod;
             }
         }
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                long long mul = pre[i][j] * suf[i][j];
-
-                res[i][j] = mul % mod;
-            }
-        }
-        
         return res;
     }
 };
