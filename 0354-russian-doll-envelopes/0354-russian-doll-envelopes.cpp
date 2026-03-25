4578f4fa6138933ad074cac30166a90f427473d9
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-03-25 09:58:21 +0530

    Time: 47 ms (40.51%), Space: 81.8 MB (35.53%) - LeetHub

---
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    static bool cmp(vector<int>& a, vector<int>& b) {
+        if (a[0] == b[0]) {
+            return a[1] > b[1];
+        }
+        return a[0] < b[0];
+    }
+    int maxEnvelopes(vector<vector<int>>& envelopes) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+
+        int n = envelopes.size();
+        sort(envelopes.begin(), envelopes.end(), cmp);
+        vector<int> lis;
+
+        for (int i = 0; i < n; i++) {
+            auto it = lower_bound(lis.begin(), lis.end(), envelopes[i][1]);
+            if (it == lis.end()) {
+                lis.push_back(envelopes[i][1]);
+            } else {
+                *it = envelopes[i][1];
+            }
+        }
+        return lis.size();
+    }
+};
