17a98f0415949430ad7d273924f4dbe81be9e72f
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-03-25 19:49:43 +0530

    Time: 62 ms (31.47%), Space: 50.2 MB (11.5%) - LeetHub

--- 0887-super-egg-drop/0887-super-egg-drop.cpp
+++ 0887-super-egg-drop/0887-super-egg-drop.cpp
@@ -1,13 +1,25 @@
 class Solution {
 public:
     int superEggDrop(int k, int n) {
-        vector<int> dp(k + 1);
+        // vector<int> dp(k + 1);
+
+        // int m = 0;
+        // while (dp[k] < n) {
+        //     m++;
+        //     for (int e = k; e > 0; e--) {
+        //         dp[e] = 1 + dp[e] + dp[e - 1];
+        //     }
+        // }
+
+        // return m;
+
+        vector<vector<int>> dp(n + 1, vector<int>(k + 1, 0));
 
         int m = 0;
-        while (dp[k] < n) {
+        while (dp[m][k] < n) {
             m++;
-            for (int e = k; e > 0; e--) {
-                dp[e] = 1 + dp[e] + dp[e - 1];
+            for (int e = 1; e <= k; e++) {
+                dp[m][e] = dp[m - 1][e - 1] + dp[m - 1][e] + 1;
             }
         }
 
