587f017f94dc090b81c5cac2e24c934866dd1ec6
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-03-25 10:14:17 +0530

    Time: 49 ms (57.54%), Space: 15.4 MB (18.01%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,5 +1,5 @@
 class Solution {
-#define ll long long int    
+#define ll long long int
 public:
     int numSquares(int n) {
         vector<ll> dp(n + 1, n);
@@ -7,7 +7,7 @@ public:
 
         for (ll i = 1; i <= n; i++) {
             for (ll j = 1; j * j <= i; j++) {
-                dp[i] = min(dp[i], dp[i - j * j] + 1);
+                dp[i] = min(dp[i], 1 + dp[i - j * j]);
             }
         }
 
