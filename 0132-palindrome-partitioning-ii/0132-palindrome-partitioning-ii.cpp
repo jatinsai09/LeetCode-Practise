f634bfbbc3bcd4e26aaf5dec948885e64206f9a0
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-03-25 11:15:22 +0530

    Time: 41 ms (80%), Space: 62.7 MB (13.78%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
-    int dpp[2005];
-    bool isPal(string& s, int i, int j) {
-        while (i < j) {
-            if (s[i++] != s[j--]) {
-                return false;
-            }
-        }
-        return true;
-    }
-    int help(int i, int n, string& s) {
-        if (i == n) {
-            return 0;
-        }
+    // int dpp[2005];
+    // bool isPal(string& s, int i, int j) {
+    //     while (i < j) {
+    //         if (s[i++] != s[j--]) {
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
+    // int help(int i, int n, string& s) {
+    //     if (i == n) {
+    //         return 0;
+    //     }
 
-        if (dpp[i] != -1) {
-            return dpp[i];
-        }
+    //     if (dpp[i] != -1) {
+    //         return dpp[i];
+    //     }
 
-        int minCost = INT_MAX;
+    //     int minCost = INT_MAX;
 
-        for (int k = i; k < n; k++) {
-            if (isPal(s, i, k)) {
-                int cost = 1 + help(k + 1, n, s);
-                minCost = min(cost, minCost);
-            }
-        }
-        return dpp[i] = minCost;
-    }
+    //     for (int k = i; k < n; k++) {
+    //         if (isPal(s, i, k)) {
+    //             int cost = 1 + help(k + 1, n, s);
+    //             minCost = min(cost, minCost);
+    //         }
+    //     }
+    //     return dpp[i] = minCost;
+    // }
     int minCut(string s) {
         int n = s.size();
-        memset(dpp, -1, sizeof(dpp));
+        //memset(dpp, -1, sizeof(dpp));
         vector<vector<int>> pal(n + 1, vector<int>(n + 1, 0));
         int dp[n + 1];
         dp[n] = 0;
