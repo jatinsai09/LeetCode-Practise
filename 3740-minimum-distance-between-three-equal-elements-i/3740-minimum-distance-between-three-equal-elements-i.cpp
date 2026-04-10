087522d96ceb0eeb1d2bb450a5810ce54219157d
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-04-10 13:49:11 +0530

    Time: 3 ms (71.85%), Space: 28.7 MB (79.62%) - LeetHub

--- 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.cpp
+++ 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.cpp
@@ -4,15 +4,17 @@ public:
         int res = INT_MAX, n = nums.size();
 
         for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) 
-            {
-                if(nums[i] == nums[j]) {
-                    for (int k = j + 1; k < n; k++) 
-                    {
-                        if(nums[k] == nums[j]) {
-                            res = min(res, 2 * (k - i));
-                        }
+            for (int j = i + 1; j < n; j++) {
+                if(nums[i] != nums[j]) {
+                    continue;
+                }
+
+                for (int k = j + 1; k < n; k++) {
+                    if(nums[k] != nums[j]) {
+                        continue;
                     }
+
+                    res = min(res, (k - i) * 2);
                 }
             }
         }
