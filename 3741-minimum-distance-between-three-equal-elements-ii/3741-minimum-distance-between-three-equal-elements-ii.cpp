10fa2ea9e0cdb6a8642a36db129a343dbd4f069f
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-04-11 09:24:58 +0530

    Time: 377 ms (63%), Space: 334.9 MB (79%) - LeetHub

--- 3741-minimum-distance-between-three-equal-elements-ii/3741-minimum-distance-between-three-equal-elements-ii.cpp
+++ 3741-minimum-distance-between-three-equal-elements-ii/3741-minimum-distance-between-three-equal-elements-ii.cpp
@@ -2,14 +2,16 @@ class Solution {
 public:
     int minimumDistance(vector<int>& a) {
         int n = a.size(), res = INT_MAX;
-        vector<vector<int>> ind(n + 1);
+        vector<vector<int>> ind(n + 1, vector<int>(2, -1));
 
         for (int i = 0; i < n; i++) {
-            int f = ind[a[i]].size();
-            if(f > 1) {
-                res = min(res, 2 * (i - ind[a[i]][f - 2]));
+
+            if(ind[a[i]][0] != -1) {
+                res = min(res, 2 * (i - ind[a[i]][0]));
             }
-            ind[a[i]].push_back(i);
+            
+            ind[a[i]][0] = ind[a[i]][1];
+            ind[a[i]][1] = i;
         }
 
         return (res == INT_MAX ? -1 : res);
