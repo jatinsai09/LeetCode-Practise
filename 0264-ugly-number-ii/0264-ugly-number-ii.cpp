3f87768c54058e4b3c718a2b2658a4e9a42472f4
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-04-13 23:39:20 +0530

    Time: 1 ms (87.52%), Space: 11.4 MB (50.68%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -1,18 +1,32 @@
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        long cur = 1;
-        set<long> s = {1};
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
 
-        for (int i = 0; i < n; i++) {
-            cur = *s.begin();
-            s.erase(begin(s));
-
-            s.insert(cur * 2);
-            s.insert(cur * 3);
-            s.insert(cur * 5);
+        if (n == 1) {
+            return 1;
         }
 
-        return cur;
+        int p2 = 0, p3 = 0, p5 = 0;
+        vector<int> v(n);
+        v[0] = 1;
+
+        for (int i = 1; i < n; i++) {
+            v[i] = min({v[p2] * 2, v[p3] * 3, v[p5] * 5});
+
+            if (v[i] == v[p2] * 2) {
+                ++p2;
+            }
+            if (v[i] == v[p3] * 3) {
+                ++p3;
+            }
+            if (v[i] == v[p5] * 5) {
+                ++p5;
+            }
+        }
+
+        return v[n - 1];
     }
 };
