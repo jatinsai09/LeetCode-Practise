5ef51cc05a3a36f67a8803fe5ada56166b353065
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-04-18 00:34:04 +0530

    Time: 0 ms (100%), Space: 7.9 MB (70.58%) - LeetHub

---
+++ 1185-day-of-the-week/1185-day-of-the-week.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string dayOfTheWeek(int day, int month, int year) {
+        vector<string> res = {"Sunday",   "Monday", "Tuesday", "Wednesday",
+                              "Thursday", "Friday", "Saturday"};
+
+        int days = 0;
+
+        for (int y = 1971; y < year; y++) {
+            days += (y % 400 == 0 || (y % 4 == 0 && y % 100 != 0)) ? 366 : 365;
+        }
+
+        vector<int> md = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+            md[1] = 29;
+        }
+
+        for (int i = 0; i < month - 1; i++) {
+            days += md[i];
+        }
+
+        days += day;
+
+        return res[(days + 4) % 7]; // Jan 1, 1971 = Friday (index 5)
+    }
+};
