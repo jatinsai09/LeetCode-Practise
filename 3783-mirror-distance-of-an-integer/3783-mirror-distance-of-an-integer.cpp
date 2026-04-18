9cb21fe6799080fd9f98761e1d2ed97a2dea1de4
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-04-18 10:31:41 +0530

    Time: 0 ms (100%), Space: 8.4 MB (86.21%) - LeetHub

--- 3783-mirror-distance-of-an-integer/3783-mirror-distance-of-an-integer.cpp
+++ 3783-mirror-distance-of-an-integer/3783-mirror-distance-of-an-integer.cpp
@@ -3,8 +3,7 @@ public:
     int reverse(int n) {
         int rev = 0;
         while (n) {
-            int r = n % 10;
-            rev = rev * 10 + r;
+            rev = rev * 10 + (n % 10);
             n /= 10;
         }
         return rev;
