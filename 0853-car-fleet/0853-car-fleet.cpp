78b7f02cee17c323ba0d0a29f08b28c1105f3082
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-04-22 22:35:43 +0530

    Time: 56 ms (41.1%), Space: 102.4 MB (85.94%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.cpp
+++ 0853-car-fleet/0853-car-fleet.cpp
@@ -1,30 +1,28 @@
 class Solution {
-    struct Car {
-        int pos, speed;
-    };
-
+struct Car {
+    int pos;
+    double time;
+};
 public:
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
-        int n = speed.size();
-        vector<Car> cars(n);
+        int n = speed.size(), res = 0;
+        vector<Car> vp(n);
+
         for (int i = 0; i < n; i++) {
-            cars[i].pos = position[i];
-            cars[i].speed = speed[i];
+            vp[i] = {position[i], (target - position[i]) / (1.0 * speed[i])};
         }
+        sort(begin(vp), end(vp), [&](auto& a, auto& b){
+            return a.pos < b.pos;
+        });
 
-        sort(begin(cars), end(cars),
-             [&](auto& a, auto& b) { return a.pos < b.pos; });
-
-        stack<float> stk;
-        for (int i = 0; i < n; i++) {
-            float time = (target - cars[i].pos) / (float)cars[i].speed;
-
-            while (!stk.empty() && stk.top() <= time) {
-                stk.pop();
+        res = 1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (vp[i].time <= vp[i + 1].time) {
+                vp[i].time = vp[i + 1].time;
+            } else {
+                res++;
             }
-            stk.push(time);
         }
-
-        return stk.size();
+        return res;
     }
 };
