a0272f10ef4a5d445777c55341ac7e9a41fd3efc
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-04-23 19:29:19 +0530

    Time: 35 ms (81.74%), Space: 12.2 MB (32.57%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,50 +1,23 @@
 class Solution {
 public:
-    // int dpp[2005];
-    // bool isPal(string& s, int i, int j) {
-    //     while (i < j) {
-    //         if (s[i++] != s[j--]) {
-    //             return false;
-    //         }
-    //     }
-    //     return true;
-    // }
-    // int help(int i, int n, string& s) {
-    //     if (i == n) {
-    //         return 0;
-    //     }
-
-    //     if (dpp[i] != -1) {
-    //         return dpp[i];
-    //     }
-
-    //     int minCost = INT_MAX;
-
-    //     for (int j = i; j < n; j++) {
-    //         if (isPal(s, i, j)) {
-    //             int cost = 1 + help(j + 1, n, s);
-    //             minCost = min(cost, minCost);
-    //         }
-    //     }
-    //     return dpp[i] = minCost;
-    // }
     int minCut(string s) {
         int n = s.size();
-        //memset(dpp, -1, sizeof(dpp));
-        vector<vector<int>> pal(n + 1, vector<int>(n + 1, 0));
-        int dp[n + 1];
-        dp[n] = 0;
+        vector<vector<bool>> pal(n, vector<bool>(n, false));
+        vector<int> dp(n + 1);
 
         for (int i = n - 1; i >= 0; i--) {
-            int minCost = n - i;
+            int cost = INT_MAX;
             for (int j = i; j < n; j++) {
-                if (s[i] == s[j] && (i + 1 >= j - 1 || pal[i + 1][j - 1])) {
+                if (s[i] != s[j]) {
+                    continue;
+                }
+
+                if (i + 1 >= j - 1 || pal[i + 1][j - 1]) {
                     pal[i][j] = 1;
-                    int cost = 1 + dp[j + 1];
-                    minCost = min(cost, minCost);
+                    cost = min(cost, 1 + dp[j + 1]);
                 }
             }
-            dp[i] = minCost;
+            dp[i] = cost;
         }
 
         return dp[0] - 1;
