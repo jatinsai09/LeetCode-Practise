726033410690a682f16f03827a02c612fbd7cc62
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-04-24 08:47:54 +0530

    Time: 0 ms (100%), Space: 9.1 MB (65.27%) - LeetHub

--- 2833-furthest-point-from-origin/2833-furthest-point-from-origin.cpp
+++ 2833-furthest-point-from-origin/2833-furthest-point-from-origin.cpp
@@ -3,7 +3,7 @@ public:
     int furthestDistanceFromOrigin(string moves) {
         int buf = 0, p = 0;
 
-        for (auto &c : moves) {
+        for (const auto &c : moves) {
             buf += (c == '_');
             p += (c == 'R') - (c == 'L');
         }
