ddf4fdc65dff072e198e0610421a702ee9312f6c
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-04-28 11:23:07 +0530

    Time: 0 ms (100%), Space: 136.8 MB (88.32%) - LeetHub

--- 3914-minimum-operations-to-make-array-non-decreasing/3914-minimum-operations-to-make-array-non-decreasing.cpp
+++ 3914-minimum-operations-to-make-array-non-decreasing/3914-minimum-operations-to-make-array-non-decreasing.cpp
@@ -6,7 +6,7 @@ public:
 
         for (int i = 1; i < n; i++) {
             if (nums[i] < nums[i - 1]) {
-                res += (nums[i - 1] - nums[i]);
+                res += nums[i - 1] - nums[i];
             }
         }
         return res;
