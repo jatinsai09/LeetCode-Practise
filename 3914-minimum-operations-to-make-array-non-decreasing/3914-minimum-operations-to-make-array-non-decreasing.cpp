54d65e1c1d3210c3eb800189782ff08057cbe2db
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-04-29 19:52:35 +0530

    Time: 3 ms (59.45%), Space: 136.8 MB (87.73%) - LeetHub

--- 3914-minimum-operations-to-make-array-non-decreasing/3914-minimum-operations-to-make-array-non-decreasing.cpp
+++ 3914-minimum-operations-to-make-array-non-decreasing/3914-minimum-operations-to-make-array-non-decreasing.cpp
@@ -5,10 +5,9 @@ public:
         ll n = nums.size(), res = 0;
 
         for (int i = 1; i < n; i++) {
-            if (nums[i] < nums[i - 1]) {
-                res += nums[i - 1] - nums[i];
-            }
+            res += max(0LL, (ll)nums[i - 1] - nums[i]);
         }
+
         return res;
     }
 };
