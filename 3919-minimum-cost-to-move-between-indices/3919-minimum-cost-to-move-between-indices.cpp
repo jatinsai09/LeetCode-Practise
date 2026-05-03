e6cf4603d09936f6359755bdf1f8267526ed8f67
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-05-03 11:19:13 +0530

    Time: 7 ms (95.95%), Space: 231.6 MB (68.4%) - LeetHub

---
+++ 3919-minimum-cost-to-move-between-indices/3919-minimum-cost-to-move-between-indices.cpp
@@ -0,0 +1,36 @@
+class Solution {
+#define ll long long
+public:
+    vector<int> minCost(vector<int>& nums, vector<vector<int>>& queries) {
+        int n = nums.size();
+        vector<ll> f(n), b(n);
+
+        for (int i = 0; i + 1 < n; i++) {
+            if (i == 0 || nums[i + 1] + nums[i - 1] < 2 * nums[i]) {
+                f[i + 1] = f[i] + 1;
+            } else {
+                f[i + 1] = f[i] + nums[i + 1] - nums[i];
+            }
+        }
+
+        for (int i = 1; i < n; i++) {
+            if (i == n - 1 || 2 * nums[i] <= nums[i + 1] + nums[i - 1]) {
+                b[i] = b[i - 1] + 1;
+            } else {
+                b[i] = b[i - 1] + nums[i] - nums[i - 1];
+            }
+        }
+        
+        vector<int> res;
+        for (auto &q: queries) {
+            int st = q[0], en = q[1];
+            if (st < en) {
+                res.push_back(f[en] - f[st]);
+            } else {
+                res.push_back(b[st] - b[en]);
+            }
+        }
+
+        return res;
+    }
+};
