a658cfbc6186ddfc820758266c7bc9dd73e9c317
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-05-03 11:50:05 +0530

    Time: 0 ms (100%), Space: 8.6 MB (19.23%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,15 +1,53 @@
 class Solution {
 public:
+    vector<int> build(string pattern) {
+        int n = pattern.size();
+        vector<int> lps(n);
+
+        int len = 0, i = 1;
+        while (i < n) {
+            if (pattern[i] == pattern[len]) {
+                lps[i++] = ++len;
+            } else { 
+                if (len == 0) {
+                    lps[i++] = 0;
+                } else {
+                    len = lps[len - 1];
+                }
+            }
+        }
+
+        return lps;
+    }
     bool rotateString(string s, string goal) {
         if (s.size() != goal.size()) {
             return false;
         }
 
-        s += s;
+        string text = s + s;
+        vector<int> lps = build(goal);
 
-        if (s.find(goal) != string::npos) {
-            return true;
+        int i = 0, j = 0;
+
+        while (i < text.size()) {
+            if (text[i] == goal[j]) {
+                i++;
+                j++;
+            }
+
+            if (j == goal.size()) {
+                return true;
+            }
+
+            if (i < text.size() && text[i] != goal[j]) {
+                if (j == 0) {
+                    i++;
+                } else {
+                    j = lps[j - 1];
+                }
+            }
         }
+        
         return false;
     }
 };
