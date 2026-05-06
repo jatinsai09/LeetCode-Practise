e48b21c7975fbfc7a0aa0264f6414269374720f4
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-05-06 08:01:09 +0530

    Time: 19 ms (25.31%), Space: 58.7 MB (12.04%) - LeetHub

---
+++ 1861-rotating-the-box/1861-rotating-the-box.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<vector<char>> rotateTheBox(vector<vector<char>>& box) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+
+        int m = box.size(), n = box[0].size();
+        vector<vector<char>> res(m, vector<char>(n));
+        int c = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                res[i][j] = box[i][j];
+                if (box[i][j] == '#') {
+                    c++;
+                    res[i][j] = '.';
+                } else if (box[i][j] == '*') {
+                    for (int k = j - 1; c > 0; k--, c--) {
+                        res[i][k] = '#';
+                    }
+                }
+                
+                if (j == n - 1 && c > 0) {
+                    for (int k = j; c > 0; k--, c--) {
+                        res[i][k] = '#';
+                    }
+                }
+            }
+        }
+        vector<vector<char>> ans(n, vector<char>(m));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                ans[j][m - i - 1] = res[i][j];
+            }
+        }
+        return ans;
+    }
+};
