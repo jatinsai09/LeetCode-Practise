dd02e83cd52013f9a5cb20caeeadb221b1d78f84
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-05-07 17:56:19 +0530

    Time: 13 ms (52.66%), Space: 225.6 MB (53.19%) - LeetHub

---
+++ 3660-jump-game-ix/3660-jump-game-ix.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> maxValue(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> smn(n), pmx(n), res(n);
+        smn[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            smn[i] = min(smn[i + 1], nums[i]);
+        }
+        pmx[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            pmx[i] = max(pmx[i - 1], nums[i]);
+        }
+
+        res[n - 1] = pmx[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            res[i] = pmx[i];
+            if (pmx[i] > smn[i + 1]) {
+                res[i] = max(res[i], res[i + 1]);
+            }
+        }
+        return res;
+    }
+};
