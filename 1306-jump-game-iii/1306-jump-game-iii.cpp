48dc32588cedb8977df9433ed24bb223b73fc853
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-05-17 10:41:30 +0530

    Time: 0 ms (100%), Space: 36.5 MB (60.85%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,13 +1,31 @@
 class Solution {
 public:
-    unordered_set<int> vis;
     bool canReach(vector<int>& arr, int st) {
-        if (st < 0 || st >= arr.size() || vis.count(st)) {
-            return false;
-        }
-        vis.insert(st);
+        int n = arr.size();
+        vector<int> vis(n);
+        queue<int> q;
 
-        return arr[st] == 0 || canReach(arr, st + arr[st]) ||
-               canReach(arr, st - arr[st]);
+        q.push(st);
+        vis[st] = 1;
+        while (!q.empty()) {
+            int cur = q.front();
+            q.pop();
+
+            if (!arr[cur]) {
+                return true;
+            }
+
+            int l = cur - arr[cur], r = cur + arr[cur];
+            if (l >= 0 && !vis[l]) {
+                q.push(l);
+                vis[l] = 1;
+            }
+            if (r < n && !vis[r]) {
+                q.push(r);
+                vis[r] = 1;
+            }
+        }
+
+        return false;
     }
 };
