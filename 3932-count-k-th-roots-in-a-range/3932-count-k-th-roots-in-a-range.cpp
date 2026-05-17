42f023e11adbaaeadf6fac3a1d87ff5344e8f3b0
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-05-17 13:29:50 +0530

    Time: 0 ms (100%), Space: 8.6 MB (82.2%) - LeetHub

---
+++ 3932-count-k-th-roots-in-a-range/3932-count-k-th-roots-in-a-range.cpp
@@ -0,0 +1,60 @@
+class Solution {
+#define ll long long int
+public:
+    ll power(ll base, ll exp, ll lim) {
+        ll res = 1;
+        while (exp--) {
+            res *= base;
+            if (res > lim) {
+                return lim + 1;
+            }
+        }
+        return res;
+    }
+
+    int lowerBound(int l, int r, int k) {
+        int low = 0, high = r, res = -1;
+        while (low <= high) {
+            int m = low + (high - low) / 2;
+            ll mpk = power(m, k, r);
+
+            if (mpk >= l) {
+                res = m;
+                high = m - 1;
+            } else {
+                low = m + 1;
+            } 
+        } 
+        return res;
+    }
+
+    int upperBound(int l, int r, int k) {
+        int low = 0, high = r, res = -1;
+        while (low <= high) {
+            int m = low + (high - low) / 2;
+            ll mpk = power(m, k, r);
+
+            if (mpk <= r) {
+                res = m;
+                low = m + 1;
+            } else {
+                high = m - 1;
+            } 
+        } 
+        return res;
+    }
+    int countKthRoots(int l, int r, int k) {
+        if (k == 1) {
+            return r - l + 1;
+        }
+
+        int lo = lowerBound(l, r, k);
+        int hi = upperBound(l, r, k);
+
+        if (lo > hi) {
+            return 0;
+        }
+
+        return hi - lo + 1;
+    }
+};
