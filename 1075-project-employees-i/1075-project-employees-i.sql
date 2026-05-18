cc80cb62d9d939b6b14be4991f98597fc04b117b
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-05-18 19:21:36 +0530

    Time: 615 ms (12.74%), Space: 0B (100%) - LeetHub

---
+++ 1075-project-employees-i/1075-project-employees-i.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT
+    p.project_id, 
+    round(avg(e.experience_years),2) as average_years
+FROM project p
+JOIN employee e
+ON p.employee_id = e.employee_id
+group by p.project_id;
