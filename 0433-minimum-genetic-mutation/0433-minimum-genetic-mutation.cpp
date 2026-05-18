18f37a85507013cd000f28138ec10950d42c5527
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-05-18 12:36:57 +0530

    Time: 0 ms (100%), Space: 9 MB (43.98%) - LeetHub

---
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int minMutation(string startGene, string endGene, vector<string>& bank) {
+        unordered_set<string> st(begin(bank), end(bank));
+        if (st.find(endGene) == st.end()) {
+            return -1;
+        }
+
+        vector<char> v = {'A', 'C', 'G', 'T'};
+        queue<string> q;
+        q.push(startGene);
+        int c = 0;
+        while (!q.empty()) {
+            int sz = q.size();
+            while (sz--) {
+                string cur = q.front();
+                q.pop();
+
+                if (cur == endGene) {
+                    return c;
+                }
+
+                for (int j = 0; j < cur.size(); j++) {
+                    for (auto &c: v) {
+                        if (c == cur[j]) {
+                            continue;
+                        }
+
+                        string next = cur.substr(0, j);
+                        next += c;
+                        next += cur.substr(j + 1);
+
+                        if (st.count(next)) {
+                            q.push(next);
+                            st.erase(next);
+                        }
+                    }
+                }
+            }
+            c++;
+        }
+        return -1;
+    }
+};
