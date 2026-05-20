16a73a3115c5bf8e224fe4308c7d33159a88da64
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-05-20 11:35:43 +0530

    Time: 0 ms (100%), Space: 85.5 MB (97.54%) - LeetHub

--- 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.cpp
+++ 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.cpp
@@ -1,18 +1,17 @@
 class Solution {
+#define ll long long int
 public:
     vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
-        int n = A.size(), c = 0;
-        vector<int> res(n), f(n + 1);
+        int n = A.size();
+        vector<int> res(n);
+        ll msk_a = 0, msk_b = 0;
 
         for (int i = 0; i < n; i++) {
-            if (++f[A[i]] == 2) {
-                c++;
-            }
-            if (++f[B[i]] == 2) {
-                c++;
-            }
-            res[i] = c;
+            msk_a |= 1LL << A[i];
+            msk_b |= 1LL << B[i];
+            res[i] = __builtin_popcountll(msk_a & msk_b);
         }
+
         return res;
     }
 };
