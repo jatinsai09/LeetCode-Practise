61ae3254dca02032b167700a128231dc42745e1f
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-05-25 18:58:21 +0530

    Time: 0 ms (100%), Space: 44.8 MB (46.43%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+
+        sort(begin(g), end(g));
+        sort(begin(s), end(s));
+
+        int i = 0, j = 0;
+        while (i < g.size() && j < s.size()) {
+            if (s[j] >= g[i]) {
+                i++;
+                j++;
+            } else {
+                j++;
+            }
+        }
+        return i;
+    }
+};
