5e88f0613b128f3ff9c775bf01d046722d69bee5
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-05-28 14:11:12 +0530

    Time: 1679 ms (11.45%), Space: 0B (100%) - LeetHub

--- 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -3,7 +3,7 @@ SELECT q1.person_name
 FROM Queue q1
 JOIN Queue q2 
     ON q1.turn >= q2.turn
-GROUP BY q1.person_id, q1.person_name
+GROUP BY q1.person_id
 HAVING SUM(q2.weight) <= 1000
 ORDER BY SUM(q2.weight) DESC
 LIMIT 1;
