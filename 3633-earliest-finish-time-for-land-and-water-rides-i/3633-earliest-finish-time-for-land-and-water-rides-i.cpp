9523d313afb91de0299df53e60178d73a509e13a
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-06-02 10:33:44 +0530

    Time: 0 ms (100%), Space: 91.8 MB (59.92%) - LeetHub

---
+++ 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int earliestFinishTime(vector<int>& ls, vector<int>& ld, vector<int>& ws,
+                           vector<int>& wd) {
+        int res = 5000;
+        int n = ls.size(), m = ws.size();
+
+        int f1 = 5000, f2 = 5000;
+        for (int i = 0; i < n; i++) {
+            f1 = min(f1, ls[i] + ld[i]);
+        }
+
+        for (int i = 0; i < m; i++) {
+            f2 = min(f2, ws[i] + wd[i]);
+        }
+
+        for (int i = 0; i < m; i++) {
+            if (ws[i] > f1) {
+                res = min(res, f1 + (ws[i] - f1) + wd[i]);
+            }
+            else {
+                res = min(res, f1 + wd[i]);
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (ls[i] > f2) {
+                res = min(res, f2 +  (ls[i] - f2) + ld[i]);
+            }
+            else {
+                res = min(res, f2 + ld[i]);
+            }
+        }
+
+        return res;
+    }
+};
