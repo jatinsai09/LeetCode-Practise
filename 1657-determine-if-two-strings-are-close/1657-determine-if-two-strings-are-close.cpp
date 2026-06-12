120d23fea1202154cebefdb1963de822d81a35c2
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-06-12 14:38:39 +0530

    Time: 10 ms (56.47%), Space: 23.3 MB (95.95%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -5,17 +5,24 @@ public:
         cin.tie(0);
         cout.tie(0);
 
-        vector<int> f1(26, 0), f2(26, 0), v1(26, 0), v2(26, 0);
+        vector<int> f1(26, 0), f2(26, 0);
 
         for (char c : word1) {
-            f1[c - 'a']++, v1[c - 'a'] = 1;
+            f1[c - 'a']++;
         }
         for (char c : word2) {
-            f2[c - 'a']++, v2[c - 'a'] = 1;
+            f2[c - 'a']++;
         }
+
+        for (int i = 0; i < 26; i++) {
+            if ((f1[i] > 0) != (f2[i] > 0)) {
+                return false;
+            }
+        }
+
         sort(begin(f1), end(f1));
         sort(begin(f2), end(f2));
 
-        return f1 == f2 && v1 == v2;
+        return f1 == f2;
     }
 };
