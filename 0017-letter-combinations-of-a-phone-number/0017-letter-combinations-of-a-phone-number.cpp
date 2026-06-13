eac195504dc8c2e12772a17e3760b01780db4f7f
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-06-13 12:31:40 +0530

    Time: 0 ms (100%), Space: 9.2 MB (76.69%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,33 +1,26 @@
 class Solution {
 public:
-    void f(string digits, vector<string>& v, vector<string>& res, string s,
-           int l) {
+    void f(string& digits, vector<string>& v, vector<string>& res, string s, int l) {
         if (l == digits.size()) {
             res.push_back(s);
             return;
         }
 
         int n = digits[l] - '0';
-        for (int i = 0; i < v[n].size(); i++) {
-            s += v[n][i];
+        for (auto &c: v[n]) {
+            s += c;
             f(digits, v, res, s, l + 1);
             s.pop_back();
         }
     }
 
     vector<string> letterCombinations(string digits) {
-        if (digits.size() == 0) {
-            return {};
-        }
-
-        vector<string> v = {"",    "",    "abc",  "def", "ghi",
+        vector<string> v = {"",    "", "abc",  "def", "ghi",
                             "jkl", "mno", "pqrs", "tuv", "wxyz"};
 
-        int l = 0;
         vector<string> res;
-        string s = "";
 
-        f(digits, v, res, s, l);
+        f(digits, v, res, "", 0);
 
         return res;
     }
