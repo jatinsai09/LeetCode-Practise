8c368019d79faf98cff38b248ae3ff493519f1a2
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-06-13 23:20:55 +0530

    Time: 0 ms (100%), Space: 9 MB (40.03%) - LeetHub

---
+++ 0763-partition-labels/0763-partition-labels.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+
+        unordered_map<char, int> li;
+        int n = s.size();
+
+        for (int i = 0; i < n; i++) {
+            li[s[i]] = i;
+        }
+        
+        vector<int> res;
+        int prev = -1, maxi = 0;
+        for (int i = 0; i < n; i++) {
+            maxi = max(maxi, li[s[i]]);
+            if (maxi == i) {
+                res.push_back(maxi - prev);
+                prev = maxi;
+            }
+        }
+        return res;
+    }
+};
