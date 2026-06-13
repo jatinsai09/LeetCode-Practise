860292e82611bd7bc01dddaee155a3a27331cff1
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-06-13 12:18:02 +0530

    Time: 0 ms (100%), Space: 14.8 MB (26.21%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.cpp
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         int n = rooms.size(), seen = 0;
+
         queue<int> q;
         vector<int> vis(n);
         q.push(0);
         vis[0] = 1;
-
         while (!q.empty()) {
             int u = q.front();
             q.pop();
