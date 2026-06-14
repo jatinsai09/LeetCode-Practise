3db04c4a35349b3d374566c90185760c9b7e4a7c
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-06-14 15:07:32 +0530

    Time: 0 ms (100%), Space: 18.8 MB (26.1%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -9,34 +9,40 @@
  * };
  */
 class Solution {
+#define pil pair<int, ListNode*>
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         ios_base::sync_with_stdio(0);
         cin.tie(0);
         cout.tie(0);
 
-        priority_queue<int, vector<int>, greater<int>> q;
+        priority_queue<pil, vector<pil>, greater<pil>> pq;
 
         for (auto& l : lists) {
-            while (l) {
-                q.push(l->val);
-                l = l->next;
+            if (l) {
+                pq.push({l->val, l});
             }
         }
 
-        if (q.empty()) {
+        if (pq.empty()) {
             return NULL;
         }
 
         ListNode* dummy = new ListNode(0);
 
         ListNode* cur = dummy;
-        while (!q.empty()) {
-            cur->next = new ListNode(q.top());
-            q.pop();
+        while (!pq.empty()) {
+            ListNode *node = pq.top().second;
+            pq.pop();
+
+            cur->next = node;
             cur = cur->next;
+
+            if (node->next) {
+                pq.push({node->next->val, node->next});
+            }
         }
-        
+
         return dummy->next;
     }
 };
