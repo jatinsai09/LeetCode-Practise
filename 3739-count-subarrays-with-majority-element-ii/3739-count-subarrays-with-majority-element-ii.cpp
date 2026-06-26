28ec64d20bc75ffd2c9f77cec10767be4c501f7f
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-06-26 08:46:48 +0530

    Time: 12 ms (82.12%), Space: 113.1 MB (54.3%) - LeetHub

---
+++ 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+#define ll long long    
+public:
+    long long countMajoritySubarrays(vector<int>& a, int target) {
+        ll n = a.size(), res = 0;
+
+        vector<ll> f(2 * n + 12, 0), p(2 * n + 12, 0);
+        ll off = n + 5, s = 0;
+        
+        p[off] = f[off] = 1;
+        for (int i = 0; i < n; i++) {
+            s += (a[i] == target ? 1 : -1);
+            
+            res += p[s + off - 1];
+            f[s + off]++;
+            p[s + off] = p[s + off - 1] + f[s + off];
+        }
+        return res;
+    }
+};
