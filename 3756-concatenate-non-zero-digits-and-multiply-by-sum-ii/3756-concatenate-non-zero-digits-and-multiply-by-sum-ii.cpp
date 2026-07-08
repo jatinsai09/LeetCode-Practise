86c1920d6959adb8de3e2e070022ded275b5ad6b
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-07-08 06:33:06 +0530

    Time: 92 ms (23.32%), Space: 172.6 MB (23.32%) - LeetHub

---
+++ 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
@@ -0,0 +1,45 @@
+class Solution {
+#define ll long long int 
+const ll mod = 1e9 + 7;    
+public:
+    vector<int> sumAndMultiply(string s, vector<vector<int>>& queries) {
+        int n = s.size();
+        vector<int> res, ind, d;
+
+        for(int i = 0; i < n; i++) {
+            if(s[i] != '0') {
+                ind.push_back(i);
+                d.push_back(s[i] - '0');
+            }
+        }
+
+        int sz = ind.size();
+        vector<ll> m(sz + 1, 1), pre(sz + 1), ps(sz + 1);
+
+        for (int i = 1; i <= sz; i++) {
+            m[i] = (m[i - 1] * 10) % mod;
+            pre[i] = (pre[i - 1] * 10 + d[i - 1]) % mod;
+            ps[i] = ps[i - 1] + d[i - 1];
+        }
+
+        for(const auto& q: queries) {
+            int l = q[0], r = q[1];
+
+            int lo = lower_bound(begin(ind), end(ind), l) - begin(ind);
+            int hi = upper_bound(begin(ind), end(ind), r) - begin(ind) - 1;
+            if(lo > hi) {
+                res.push_back(0);
+                continue;
+            }
+
+            ll c = hi - lo + 1;
+
+            ll g = (pre[hi + 1] - (pre[lo] * m[c]) % mod + mod) % mod;
+            ll s = ps[hi + 1] - ps[lo];
+            ll got = (g * (s % mod)) % mod;
+            
+            res.push_back(got);
+        }
+        return res;
+    }
+};
