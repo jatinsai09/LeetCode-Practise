36be1919bae53f61ce49d70139f2aafb57996878
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-07-08 06:38:58 +0530

    Time: 80 ms (27.46%), Space: 172.5 MB (23.32%) - LeetHub

--- 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
+++ 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
@@ -3,6 +3,10 @@ class Solution {
 const ll mod = 1e9 + 7;    
 public:
     vector<int> sumAndMultiply(string s, vector<vector<int>>& queries) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(0);
+        cout.tie(0);
+
         int n = s.size();
         vector<int> res, ind, d;
 
