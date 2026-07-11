226cc2d0b0f57712976652d57f1359f6a89006fe
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-07-11 20:03:51 +0530

    Time: 0 ms (100%), Space: 11.5 MB (85.41%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -4,6 +4,7 @@ public:
         ios_base::sync_with_stdio(0);
         cin.tie(0);
         cout.tie(0);
+        
         int n = grid.size(), m = grid[0].size();
         if (grid[0][0] == 1) {
             return 0;
@@ -14,10 +15,11 @@ public:
         for (int i = 0; i < n; i++) {
             cur[0] = (grid[i][0] ? 0 : cur[0]);
             for (int j = 1; j < m; j++) {
-                cur[j] += cur[j - 1];
                 if (grid[i][j]) {
                     cur[j] = 0;
+                    continue;
                 }
+                cur[j] += cur[j - 1];
             }
         }
 
