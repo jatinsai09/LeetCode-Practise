44e097ae2bbd1561345322dfa6dcba037aaf1297
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-07-12 10:18:58 +0530

    Time: 12 ms (99.8%), Space: 39.8 MB (83.39%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -2,15 +2,27 @@ class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
         int n = arr.size();
-        vector<int> s = arr;
-
-        sort(begin(s), end(s));
-        s.erase(unique(begin(s), end(s)), end(s));
-
-        for (int i = 0; i < n; i++) {
-            arr[i] = lower_bound(begin(s), end(s), arr[i]) - begin(s) + 1;
+        if (n == 0) {
+            return {};
         }
 
-        return arr;
+        vector<pair<int, int>> vp;
+
+        for (int i = 0; i < n; i++) {
+            vp.push_back({arr[i], i});
+        }
+        sort(begin(vp), end(vp), [&](auto& a, auto& b){
+            return a.first < b.first;
+        });
+
+        vector<int> res(n);
+        res[vp[0].second] = 1;
+        for (int i = 1; i < n; i++) {
+            int cur = vp[i].first, prev = vp[i - 1].first;
+
+            res[vp[i].second] = res[vp[i - 1].second] + (prev != cur);
+        }
+
+        return res;
     }
 };
