d7b15ccd8139f330410ae50c8cad8790f0e486d4
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-07-12 23:42:40 +0530

    Time: 327 ms (5.01%), Space: 102.5 MB (58.66%) - LeetHub

---
+++ 3989-maximum-consistent-columns-in-a-grid/3989-maximum-consistent-columns-in-a-grid.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxConsistentColumns(vector<vector<int>>& grid, int limit) {
+        int n = grid.size(), m = grid[0].size();
+        vector<int> dp(m, 1);
+        int res = 1;
+
+        for (int b = 0; b < m; b++) {
+            for (int a = 0; a < b; a++) {
+                if (dp[a] + 1 > dp[b]) {
+                    bool yes = true;
+                    for (int i = 0; i < n; i++) {
+                        yes &= (abs(grid[i][b] - grid[i][a]) <= limit);
+                    }
+
+                    if (yes) {
+                        dp[b] = dp[a] + 1;
+                    }
+                }
+            }
+            res = max(res, dp[b]);
+        }
+
+        return res;
+    }
+};
