a85373ca4ce2bb09111a93abe22b18b189c975f6
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-07-18 12:49:26 +0530

    Time: 55 ms (27.12%), Space: 65.7 MB (52.27%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -12,7 +12,7 @@ public:
         for (int i = 0; i < v; i++) {
             // i->it;
             // it->i;
-            for (auto& it : adj[i]) {
+            for (const auto& it : adj[i]) {
                 adjRev[it].push_back(i);
                 indegree[i]++;
             }
@@ -30,7 +30,7 @@ public:
             q.pop();
 
             safe.push_back(node);
-            for (auto& it : adjRev[node]) {
+            for (const auto& it : adjRev[node]) {
                 if (--indegree[it] == 0) {
                     q.push(it);
                 }
