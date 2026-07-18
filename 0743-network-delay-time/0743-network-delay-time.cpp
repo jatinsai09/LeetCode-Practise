07a6d0b098e74d6040911938a57133ad46c56bd6
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-07-18 07:53:44 +0530

    Time: 65 ms (98.53%), Space: 44.5 MB (44.85%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,24 +1,24 @@
 class Solution {
-
+#define pii pair<int, int>
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<vector<pair<int, int>>> adj(n + 1);
+        vector<vector<pii>> adj(n + 1);
 
-        for (auto& it : times) {
+        for (const auto& it : times) {
             int u = it[0], v = it[1], wt = it[2];
             adj[u].push_back({v, wt});
         }
 
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+        priority_queue<pii, vector<pii>, greater<>> pq;
         vector<int> dist(n + 1, 1e9);
-        
+
         pq.push({0, k});
         dist[k] = 0;
         while (!pq.empty()) {
             auto [curDist, curNode] = pq.top();
             pq.pop();
 
-            for (auto& [adjNode, wt] : adj[curNode]) {
+            for (const auto& [adjNode, wt] : adj[curNode]) {
                 if (curDist + wt < dist[adjNode]) {
                     pq.push({curDist + wt, adjNode});
                     dist[adjNode] = curDist + wt;
