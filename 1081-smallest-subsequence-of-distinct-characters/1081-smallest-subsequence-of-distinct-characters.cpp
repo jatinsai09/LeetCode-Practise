d03db607da77d6325b60c9267064ed5179f79591
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-07-19 09:07:06 +0530

    Time: 2 ms (17.88%), Space: 8.6 MB (79.99%) - LeetHub

--- 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.cpp
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.cpp
@@ -3,6 +3,7 @@ public:
     string smallestSubsequence(string s) {
         string res = "";
         int n = s.size();
+
         vector<int> last(26), vis(26);
         for(int i = 0; i < n; i++) {
             last[s[i] - 'a'] = i;
@@ -12,6 +13,7 @@ public:
             if(vis[s[i] - 'a']) {
                 continue;
             }
+            
             vis[s[i] - 'a'] = 1;
             while(!res.empty() && res.back() > s[i] && last[res.back() - 'a'] > i) {
                 vis[res.back() - 'a'] = 0;
