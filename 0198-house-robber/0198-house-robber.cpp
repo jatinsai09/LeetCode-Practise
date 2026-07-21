f14acbdbada2ce28af9d8c84160d02946cf55d30
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-07-21 13:00:30 +0530

    Time: 0 ms (100%), Space: 10.1 MB (89.91%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int n = nums.size(), prev2 = 0, prev = nums[0], cur = nums[0];
+        int n = nums.size(), prev2 = 0, prev = 0, cur;
 
-        for (int i = 1; i < n; i++) {
-            int pick = nums[i] + prev2, not_pick = prev;
+        for (const auto& i: nums) {
+            int pick = i + prev2, not_pick = prev;
 
             cur = max(pick, not_pick);
             prev2 = prev;
