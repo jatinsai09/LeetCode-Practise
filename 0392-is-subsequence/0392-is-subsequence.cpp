74e730730126a4ea5c8b18f47cae08500ef8aa93
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-07-22 17:40:22 +0530

    Time: 0 ms (100%), Space: 8.5 MB (67.96%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -4,14 +4,14 @@ public:
         ios_base::sync_with_stdio(0);
         cin.tie(0);
         cout.tie(0);
+
         int m = s.size(), n = t.size();
         int ind = 0;
         for (int i = 0; i < n; i++) {
-            if (t[i] == s[ind])
+            if (t[i] == s[ind]) {
                 ind++;
+            }
         }
-        if (ind == m)
-            return true;
-        return false;
+        return ind == m;
     }
 };
