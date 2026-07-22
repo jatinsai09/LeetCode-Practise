c1cb9c932ea944fcdafdbbcdb3dd1a12ce3d9e2a
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-07-22 09:50:11 +0530

    Time: 0 ms (100%), Space: 11.6 MB (49.36%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -5,14 +5,11 @@ public:
 
         while (mid <= high) {
             if (nums[mid] == 0) {
-                swap(nums[low], nums[mid]);
-                low++;
-                mid++;
+                swap(nums[low++], nums[mid++]);
             } else if (nums[mid] == 1) {
                 mid++;
             } else {
-                swap(nums[mid], nums[high]);
-                high--;
+                swap(nums[mid], nums[high--]);
             }
         }
     }
