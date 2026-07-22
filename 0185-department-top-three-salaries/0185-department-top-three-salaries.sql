f69ece8569c54ae68f980fa7484c5e607e047f00
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-07-22 21:38:56 +0530

    Time: 1171 ms (32.23%), Space: 0B (100%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -4,7 +4,7 @@ WITH cte as (
         d.name AS Department,
         e.name AS Employee,
         e.salary AS Salary,
-        DENSE_RANK() OVER (PARTITION BY d.name ORDER BY salary DESC) AS rnk
+        DENSE_RANK() OVER (PARTITION BY d.name ORDER BY e.salary DESC) AS rnk
     FROM Employee e
     JOIN Department d
     ON e.departmentId = d.id
