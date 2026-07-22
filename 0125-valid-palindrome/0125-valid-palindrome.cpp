5049c22ddbfb8f1802f045139b00b24c6baf11e7
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-07-22 09:48:10 +0530

    Time: 1 ms (44.1%), Space: 10.1 MB (37.42%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+
+        int start = 0, end = s.size() - 1;
+        while (start <= end) {
+            if (!isalnum(s[start])) {
+                start++;
+                continue;
+            }
+            if (!isalnum(s[end])) {
+                end--;
+                continue;
+            }
+
+            if (tolower(s[start++]) != tolower(s[end--])) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
