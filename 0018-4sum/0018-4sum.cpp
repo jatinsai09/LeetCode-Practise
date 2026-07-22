7809c2d335eb07a2e58287bb60858441ecbd49aa
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-07-22 13:26:48 +0530

    Time: 27 ms (22.13%), Space: 17.4 MB (84.73%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -6,11 +6,14 @@ public:
         sort(nums.begin(), nums.end());
 
         for (int i = 0; i < n; i++) {
-            if (i > 0 && nums[i] == nums[i - 1])
+            if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
+            }
             for (int j = i + 1; j < n; j++) {
-                if (j != i + 1 && nums[j] == nums[j - 1])
+                if (j - 1 != i && nums[j] == nums[j - 1]) {
                     continue;
+                }
+                
                 int k = j + 1, l = n - 1;
                 while (k < l) {
                     long long sum = nums[i];
@@ -21,12 +24,15 @@ public:
                     if (sum == target) {
                         vector<int> temp = {nums[i], nums[j], nums[k], nums[l]};
                         res.push_back(temp);
+
                         k++;
                         l--;
-                        while (k < l && nums[k] == nums[k - 1])
+                        while (k < l && nums[k] == nums[k - 1]) {
                             k++;
-                        while (k < l && nums[l] == nums[l + 1])
+                        }
+                        while (k < l && nums[l] == nums[l + 1]) {
                             l--;
+                        }
                     } else if (sum < target) {
                         k++;
                     } else {
