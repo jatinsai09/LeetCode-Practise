204044c9fe02b458011f84ee5c86f6e3b43cd78c
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-07-24 18:56:30 +0530

    Time: 0 ms (100%), Space: 11.9 MB (10.52%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -4,13 +4,13 @@ public:
         int n = s.size(), m = t.size();
         
         int f[256] = {0};
-        for (auto& c : t) {
+        for (const auto& c : t) {
             f[c]++;
         }
 
-        int l = 0, r = 0, ms = INT_MAX, st = -1, c = 0;
-        while (r < n) {
-            if (f[s[r]]-- > 0) {
+        int ms = INT_MAX, st = -1, c = 0;
+        for (int l = 0, r = 0; r < n; r++) {
+            if (--f[s[r]] >= 0) {
                 c++;
             }
 
@@ -19,12 +19,13 @@ public:
                     ms = r - l + 1;
                     st = l;
                 }
-                if(ms == m) break;
 
-                if (++f[s[l]] > 0) c--;
-                l++;
+                c -= (++f[s[l++]] > 0);
             }
-            r++;
+
+            if (ms == m) {
+                break;
+            }
         }
 
         return (st == -1) ? "" : s.substr(st, ms);
