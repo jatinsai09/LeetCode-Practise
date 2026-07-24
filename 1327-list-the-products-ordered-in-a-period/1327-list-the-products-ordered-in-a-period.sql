8af7efc5b9a19910723b564273f92c77d6730d7d
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-07-24 13:16:14 +0530

    Time: 707 ms (85.2%), Space: 0B (100%) - LeetHub

--- 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
+++ 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
@@ -5,6 +5,6 @@ SELECT
 FROM Products p
 JOIN Orders o
     ON p.product_id = o.product_id
-WHERE MONTH(o.order_date) = 2 AND YEAR(o.order_date) = 2020
+WHERE o.order_date LIKE "2020-02-%"
 GROUP BY p.product_id
 HAVING SUM(o.unit) >= 100;
