bfcf2f5f3689e6038a8b6d49b83422db8cc0c391
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-07-25 11:37:24 +0530

    Time: 11 ms (98.45%), Space: 11.2 MB (99.72%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
-    int dpp[111][111];
-    int help(int s, int e, vector<int>& cuts) {
-        if (s > e) {
-            return 0;
-        }
-        if (dpp[s][e] != -1) {
-            return dpp[s][e];
-        }
+    // int dpp[111][111];
+    // int help(int s, int e, vector<int>& cuts) {
+    //     if (s > e) {
+    //         return 0;
+    //     }
+    //     if (dpp[s][e] != -1) {
+    //         return dpp[s][e];
+    //     }
 
-        int res = 1e9;
-        for (int i = s; i <= e; i++) {
-            int cur = (cuts[e + 1] - cuts[s - 1]) + help(s, i - 1, cuts) +
-                      help(i + 1, e, cuts);
-            res = min(res, cur);
-        }
-        return dpp[s][e] = res;
-    }
+    //     int res = 1e9;
+    //     for (int i = s; i <= e; i++) {
+    //         int cur = (cuts[e + 1] - cuts[s - 1]) + help(s, i - 1, cuts) +
+    //                   help(i + 1, e, cuts);
+    //         res = min(res, cur);
+    //     }
+    //     return dpp[s][e] = res;
+    // }
     int minCost(int n, vector<int>& cuts) {
         sort(begin(cuts), end(cuts));
-        int m = cuts.size(), dp[m + 2][m + 2];
-        memset(dp, 0, sizeof(dp));
+        int m = cuts.size();
 
         cuts.push_back(n);
         cuts.insert(begin(cuts), 0);
 
-        for (int s = m; s >= 1; s--) {
+
+        int dp[m + 3][m + 3];
+        memset(dp, 0, sizeof(dp));
+        for (int s = m; s > 0; s--) {
             for (int e = s; e <= m; e++) {
                 int res = 1e9;
                 for (int i = s; i <= e; i++) {
@@ -36,6 +38,7 @@ public:
                 dp[s][e] = res;
             }
         }
+
         return dp[1][m];
     }
 };
