eddc2d17f36980137e3298277d21893e995095c7
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-07-25 21:31:21 +0530

    Time: 0 ms (100%), Space: 50.3 MB (34.54%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
-        int r = mat.size(), c = mat[0].size();
-        int mc, lc = 0, rc = c - 1;
+        int n = mat.size(), m = mat[0].size();
 
-        while (lc <= rc) {
-            mc = lc + (rc - lc) / 2;
-            int mr = 0;
-            for (int i = 0; i < r; i++) {
-                if (mat[mr][mc] < mat[i][mc]) {
-                    mr = i;
+        int l = 0, r = m - 1, mid;
+        while (l <= r) {
+            mid = l + (r - l) / 2;
+
+            int row = 0;
+            for (int i = 1; i < n; i++) {
+                if (mat[i][mid] > mat[row][mid]) {
+                    row = i;
                 }
             }
 
-            bool left = mc - 1 >= 0 && mat[mr][mc - 1] > mat[mr][mc];
-            bool right = mc + 1 < c && mat[mr][mc + 1] > mat[mr][mc];
+            int left = (mid ? mat[row][mid - 1] : -1);
+            int right = (mid + 1 < m ? mat[row][mid + 1] : -1);
 
-            if (!left && !right) {
-                return vector<int>{mr, mc};
-            }
-
-            if (left) {
-                rc = mc - 1;
+            if (mat[row][mid] > left && mat[row][mid] > right) {
+                return {row, mid};
+            } else if (mat[row][mid] < left) {
+                r = mid - 1;
             } else {
-                lc = mc + 1;
+                l = mid + 1;
             }
         }
-        return vector<int>{-1, -1};
+
+        return {-1, -1};
     }
 };
