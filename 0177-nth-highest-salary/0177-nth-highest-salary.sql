4d914518681eff4ca4894089de5338ab34839e3e
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-07-30 20:37:43 +0530

    Time: 431 ms (86.19%), Space: 0B (100%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -1,7 +1,5 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
-    DECLARE M INT;
-    SET M = N - 1;
   RETURN (
       # Write your MySQL query statement below.
       WITH cte AS (
