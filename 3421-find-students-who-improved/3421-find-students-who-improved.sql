bbf25b7425f1119b2f3f639be6181f2fb54586f2
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-07-30 22:22:12 +0530

    Time: 309 ms (56.96%), Space: 0B (100%) - LeetHub

--- 3421-find-students-who-improved/3421-find-students-who-improved.sql
+++ 3421-find-students-who-improved/3421-find-students-who-improved.sql
@@ -12,17 +12,17 @@ LatestExams AS (
     HAVING COUNT(exam_date) > 1
 )
 
-SELECT 
+SELECT
     f.student_id,
     f.subject,
     s1.score AS first_score,
     s2.score AS latest_score
 FROM FirstExams f
-JOIN LatestExams l 
+JOIN LatestExams l
     ON f.student_id = l.student_id AND f.subject = l.subject
-JOIN Scores s1 
+JOIN Scores s1
     ON f.student_id = s1.student_id AND f.subject = s1.subject AND f.first_date = s1.exam_date
-JOIN Scores s2 
+JOIN Scores s2
     ON l.student_id = s2.student_id AND l.subject = s2.subject AND l.latest_date = s2.exam_date
 WHERE s2.score > s1.score
 ORDER BY f.student_id, f.subject;
