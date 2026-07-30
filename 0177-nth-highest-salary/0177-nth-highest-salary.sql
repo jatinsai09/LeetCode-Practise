e0b1dcb0970c34a50e717813fc3f79a0efd4b412
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-07-30 20:35:53 +0530

    Time: 440 ms (78.15%), Space: 0B (100%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -4,9 +4,13 @@ BEGIN
     SET M = N - 1;
   RETURN (
       # Write your MySQL query statement below.
+      WITH cte AS (
+        SELECT salary,
+               DENSE_RANK() OVER (ORDER BY salary DESC) as rnk
+        FROM Employee
+      )
       SELECT DISTINCT salary
-      FROM Employee
-      ORDER BY salary DESC
-      LIMIT 1 OFFSET M
+      FROM cte
+      WHERE rnk = N
   );
 END
