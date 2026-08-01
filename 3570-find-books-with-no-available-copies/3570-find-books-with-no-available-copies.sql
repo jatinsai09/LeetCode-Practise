5a8697c7fd3e42ecbea1eb2af0dc8ffcdda5f070
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-08-01 18:18:29 +0530

    Time: 555 ms (60.17%), Space: 0B (100%) - LeetHub

--- 3570-find-books-with-no-available-copies/3570-find-books-with-no-available-copies.sql
+++ 3570-find-books-with-no-available-copies/3570-find-books-with-no-available-copies.sql
@@ -11,5 +11,5 @@ JOIN borrowing_records b
     ON l.book_id = b.book_id
 WHERE b.return_date IS NULL
 GROUP BY l.book_id
-HAVING COUNT(record_id) = current_borrowers
+HAVING COUNT(record_id) = l.total_copies
 ORDER BY current_borrowers DESC, l.title ASC;
