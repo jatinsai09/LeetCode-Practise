06746d5506551cbbfec23075c42c48c38a3be5c8
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-08-08 19:43:33 +0530

    Time: 41 ms (69.15%), Space: 97 MB (86.17%) - LeetHub

--- 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.cpp
+++ 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.cpp
@@ -3,33 +3,33 @@ public:
     vector<int> validSequence(string w1, string w2) {
         int n = w1.size(), m = w2.size();
 
-        vector<int> suf(n);
-        int c = 0, j = m - 1;
-        for (int i = n - 1; i >= 0; i--) {
-            suf[i] = c;
-            if (j >= 0 && w1[i] == w2[j]) {
-                c++;
-                j--;
-            }
-        }
-
-        vector<int> res;
-        bool can = true;
-        j = 0;
-        for (int i = 0; i < n && j < m; i++) {
+        vector<int> suf(m + 1, -1);
+        suf[m] = n;
+        for (int i = n - 1, j = m - 1; i >= 0; i--) {
             if (w1[i] == w2[j]) {
-                res.push_back(i);
-                j++;
-            } else if (can && suf[i] >= m - (j + 1)) {
-                can = false;
-                res.push_back(i);
-                j++;
+                suf[j--] = i;
+            }
+            if (j < 0) {
+                break;
             }
         }
 
-        if (j == m) {
-            return res;
+        vector<int> res(m);
+        bool can = true;
+
+        for (int i = 0, j = 0; i < n; i++) {
+            if (w1[i] == w2[j]) {
+                res[j++] = i;
+            } else if (can && i < suf[j + 1]) {
+                res[j++] = i;
+                can = false;
+            }
+
+            if (j == m) {
+                return res;
+            }
         }
+
         return {};
     }
 };
