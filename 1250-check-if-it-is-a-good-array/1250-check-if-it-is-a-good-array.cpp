845408d0a009274f71c9adadd7c5fe454a3ac340
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-08-11 23:21:40 +0530

    Time: 0 ms (100%), Space: 33.2 MB (22.84%) - LeetHub

---
+++ 1250-check-if-it-is-a-good-array/1250-check-if-it-is-a-good-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isGoodArray(vector<int>& nums) {
+        int g = nums[0];
+        for (auto &i: nums) {
+            g = __gcd(i, g);
+            if (g == 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
