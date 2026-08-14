d89dac6e207a14e1f655aa389cf4ede2b8e5d517
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-08-14 17:10:39 +0530

    Time: 736 ms (34.14%), Space: 351.8 MB (20.48%) - LeetHub

---
+++ 3589-count-prime-gap-balanced-subarrays/3589-count-prime-gap-balanced-subarrays.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    int primeSubarray(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        vector<int> p;
+        for (const auto& x : nums) {
+            bool prime = 1;
+            if (x == 1) {
+                p.push_back(0);
+                continue;
+            }
+
+            for (int i = 2; i * i <= x; i++) {
+                if (x % i == 0) {
+                    prime = 0;
+                    break;
+                }
+            }
+            p.push_back(prime);
+        }
+
+        deque<int> q;
+        multiset<int> s;
+        int res = 0;
+        for (int l = 0, r = 0; r < n; r++) {
+            if (p[r]) {
+                s.insert(nums[r]);
+                q.push_back(r);
+            }
+
+            while (!s.empty() && *s.rbegin() - *s.begin() > k) {
+                if (p[l]) {
+                    s.erase(s.find(nums[l]));
+                    q.pop_front();
+                }
+                l++;
+            }
+
+            if (q.size() >= 2) {
+                int i1 = q.back();
+                q.pop_back();
+
+                int i2 = q.back();
+                res += (i2 - l + 1);
+
+                q.push_back(i1);
+            }
+        }
+
+        return res;
+    }
+};
