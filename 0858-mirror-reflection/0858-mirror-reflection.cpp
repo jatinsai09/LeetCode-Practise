c24c22162d0992ec3f05474d9ab5be04b52939de
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-08-16 12:46:20 +0530

    Time: 0 ms (100%), Space: 7.9 MB (9.19%) - LeetHub

--- 0858-mirror-reflection/0858-mirror-reflection.cpp
+++ 0858-mirror-reflection/0858-mirror-reflection.cpp
@@ -2,23 +2,15 @@ class Solution {
 public:
 // ext * p == ref * q 
     int mirrorReflection(int p, int q) {
-        int ext = 1, ref = 1;
 
-        while (1) {
-            if (ext * p == ref * q) {
-                if ((ext & 1) || (ref & 1)) {
-                    break;
-                }
-            } else if (ext * p < ref * q) {
-                ext++;
-            } else {
-                ref++;
-            }
+        while (p % 2 == 0 && q % 2 == 0) {
+            p /= 2;
+            q /= 2;
         }
 
-        if (ext % 2 == 0) {
+        if (q % 2 == 0) {
             return 0;
-        } else if (ref % 2 == 0) {
+        } else if (p % 2 == 0) {
             return 2;
         } else {
             return 1;
