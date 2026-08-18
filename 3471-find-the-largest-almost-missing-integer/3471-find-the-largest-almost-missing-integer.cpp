525e35ee799b8ed11f62fbd70300f70e7032a40c
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-08-18 16:52:10 +0530

    Time: 0 ms (100%), Space: 29 MB (84.21%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
@@ -16,23 +16,21 @@ public:
             f[i]++;
         }
 
+        int mx = -1;
         if (k == 1) {
-            int mx = -1;
             for (const auto &i: nums) {
                 if (f[i] == 1) {
                     mx = max(mx, i);
                 }
             }
-            return mx;
         } else {
-            if (f[nums[0]] == 1 and f[nums[n - 1]] == 1) {
-                return max(nums[0], nums[n - 1]);
-            } else if (f[nums[0]] == 1) {
-                return nums[0];
-            } else if (f[nums[n - 1]] == 1) {
-                return nums[n - 1];
+            if (f[nums[0]] == 1) {
+                mx = max(mx, nums[0]);
+            }
+            if (f[nums[n - 1]] == 1) {
+                mx = max(mx, nums[n - 1]);
             }
         }
-        return -1;
+        return mx;
     }
 };
