b6309734d8bc92ca2f015d72855205d5b2d231d4
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-08-23 11:18:26 +0530

    Time: 3 ms (66.67%), Space: 14 MB (67.83%) - LeetHub

--- 1927-sum-game/1927-sum-game.cpp
+++ 1927-sum-game/1927-sum-game.cpp
@@ -1,14 +1,7 @@
 class Solution {
 public:
     bool sumGame(string num) {
-        int n = num.size(), cq = 0;
-
-        for (const auto &c: num) {
-            cq += (c == '?');
-        }
-        if (cq & 1) {
-            return true;
-        }
+        int n = num.size();
 
         int s1 = 0, s2 = 0, c1 = 0, c2 = 0;
         for (int i = 0; i < n / 2; i++) {
