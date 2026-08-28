fac09a281d3dd7b4eb52a430521a5ed7ac362b9e
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-08-28 23:25:35 +0530

    Time: 9 ms (14.42%), Space: 26 MB (97.36%) - LeetHub

--- 1267-count-servers-that-communicate/1267-count-servers-that-communicate.cpp
+++ 1267-count-servers-that-communicate/1267-count-servers-that-communicate.cpp
@@ -1,26 +1,38 @@
 class Solution {
 public:
     int countServers(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size();
+        int m = grid.size(), n = grid[0].size(), res = 0;
 
-        vector<int> rows(m, 0), cols(n, 0);
         for (int i = 0; i < m; i++) {
+            int c1 = 0;
             for (int j = 0; j < n; j++) {
-                if (grid[i][j]) {
-                    rows[i]++;
-                    cols[j]++;
+                c1 += (grid[i][j]);
+            }
+            if (c1 > 1) {
+                res += c1;
+                for (int j = 0; j < n; j++) {
+                    if (grid[i][j]) {
+                        grid[i][j] = 2;
+                    }
                 }
+            }
+        }
+
+        for (int j = 0; j < n; j++) {
+            int c1 = 0, c2 = 0;
+            for (int i = 0; i < m; i++) {
+                if (grid[i][j] == 2) {
+                    c2++;
+                } else if (grid[i][j] == 1) {
+                    c1++;
+                }
+            }
+
+            if (c1 + c2 > 1) {
+                res += c1;
             }
         }
         
-        int res = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] && (cols[j] > 1 || rows[i] > 1)) {
-                    res++;
-                }
-            }
-        }
         return res;
     }
 };
