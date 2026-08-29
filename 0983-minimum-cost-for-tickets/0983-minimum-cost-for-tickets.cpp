082daa8cf366cebefae502b8ee758da6101b072f
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-08-29 17:52:42 +0530

    Time: 0 ms (100%), Space: 13.4 MB (58.88%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -7,16 +7,17 @@ public:
 
         int cost = 0;
         queue<pair<int, int>> last7, last30;
-        for (auto& d : days) {
-            while (!last7.empty() && last7.front().first + 7 <= d) {
+        for (const auto& d : days) {
+            while (!last7.empty() && last7.front().first < d) {
                 last7.pop();
             }
-            while (!last30.empty() && last30.front().first + 30 <= d) {
+            while (!last30.empty() && last30.front().first < d) {
                 last30.pop();
             }
 
-            last7.push({d, cost + costs[1]});
-            last30.push({d, cost + costs[2]});
+            last7.push({d + 7 - 1, cost + costs[1]});
+            last30.push({d + 30 - 1, cost + costs[2]});
+
             cost = min(
                 {cost + costs[0], last7.front().second, last30.front().second});
         }
