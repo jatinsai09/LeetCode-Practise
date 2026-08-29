bfd3f8bc795c43edfc2947b86f428df1ff44523c
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-08-29 21:07:19 +0530

    Time: 0 ms (100%), Space: 11.3 MB (76.44%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -7,18 +7,15 @@ public:
 
         unordered_set<string> st(begin(wordDict), end(wordDict));
         int n = s.size();
+        
         vector<int> dp(n + 1);
         dp[n] = 1;
-
         for (int i = n - 1; i >= 0; i--) {
             string temp = "";
-            for (int j = i; j < n; j++) {
+            for (int j = i; j < n && !dp[i]; j++) {
                 temp += s[j];
-                if (dp[j + 1]) {
-                    dp[i] = (st.count(temp) > 0);
-                }
-                if (dp[i]) {
-                    break;
+                if (st.count(temp)) {
+                    dp[i] = dp[j + 1];
                 }
             }
         }
