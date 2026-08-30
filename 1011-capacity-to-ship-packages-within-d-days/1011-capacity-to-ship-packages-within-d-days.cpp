bf34cf04fa8a8ab962428beda726b02f5504112d
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-08-30 19:26:50 +0530

    Time: 7 ms (91.02%), Space: 35.1 MB (54.25%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -2,17 +2,15 @@ class Solution {
 public:
     int shipWithinDays(vector<int>& weights, int days) {
         int n = weights.size(), s = 0, mx = 0;
-        for (auto &w: weights) {
+        for (const auto &w: weights) {
             s += w;
             mx = max(mx, w);
         }
 
-        int l = mx, r = s, m;
-
         auto possible = [&](int limit) -> bool {
             int time = 1, got = 0;
 
-            for (auto &w: weights) {
+            for (const auto &w: weights) {
                 if (got + w > limit) {
                     time++;
                     got = w;
@@ -24,6 +22,7 @@ public:
             return time <= days;
         };
 
+        int l = mx, r = s, m;
         while (l <= r) {
             m = l + (r - l) / 2;
 
