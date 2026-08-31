42f4d1970dcc29265bdb6627b95c53434978e888
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-08-31 19:30:37 +0530

    Time: 24 ms (67.12%), Space: 21.1 MB (50.2%) - LeetHub

---
+++ 0690-employee-importance/0690-employee-importance.cpp
@@ -0,0 +1,36 @@
+/*
+// Definition for Employee.
+class Employee {
+public:
+    int id;
+    int importance;
+    vector<int> subordinates;
+};
+*/
+
+class Solution {
+public:
+    int getImportance(vector<Employee*> employees, int id) {
+        unordered_map<int, Employee*> mp;
+        for (auto &e: employees) {
+            mp[e->id] = e;
+        }
+
+        int res = 0;
+        queue<int> q;
+        q.push(id);
+        while (!q.empty()) {
+            int eid = q.front();
+            q.pop();
+
+            Employee* emp = mp[eid];
+            res += emp->importance;
+
+            for (auto sid: emp->subordinates) {
+                q.push(sid);
+            }
+        }
+
+        return res;
+    }
+};
