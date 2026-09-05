39145fc830da1df98ae66b367ba45c44afedc34e
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-09-05 18:21:10 +0530

    Time: 0 ms (100%), Space: 8.6 MB (63.86%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     int numDecodings(string s) {
         int n = s.size(), _1, _2;
-        if(n == 1) {
+        if (n == 1) {
             return s[0] != '0';
         }
         if (s[0] == '0') {
-            return false;
+            return 0;
         }
 
         vector<int> dp(n, 0);
@@ -15,7 +15,7 @@ public:
             _1 = (s[i] == '0' ? 0 : dp[i - 1]);
             _2 = 0;
 
-            if (s[i - 1] > '0' && (s[i - 1] - '0') * 10 + (s[i] - '0') < 27) {
+            if (s[i - 1] != '0' && (s[i - 1] - '0') * 10 + (s[i] - '0') < 27) {
                 _2 = (i >= 2 ? dp[i - 2] : 1);
             }
             dp[i] = _1 + _2;
