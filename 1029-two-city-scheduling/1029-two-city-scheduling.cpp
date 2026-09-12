5a3491031c8e1144f38d4f276b8de36081226c2f
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-09-12 18:41:19 +0530

    Time: 0 ms (100%), Space: 11.2 MB (42.49%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -7,6 +7,7 @@ public:
         for (const auto &c: costs) {
             res += c[0];
             pq.push(c[0] - c[1]);
+            
             if (pq.size() > n / 2) {
                 res -= pq.top();
                 pq.pop();
