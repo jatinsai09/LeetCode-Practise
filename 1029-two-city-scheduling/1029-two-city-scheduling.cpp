a6dff5763bfde70055e038fbe2356cc122cbc8d6
jatinsai09 <153115351+jatinsai09@users.noreply.github.com>
2026-09-12 18:48:12 +0530

    Time: 0 ms (100%), Space: 11 MB (61.77%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -3,17 +3,13 @@ public:
     int twoCitySchedCost(vector<vector<int>>& costs) {
         int n = costs.size(), res = 0;
 
-        priority_queue<int> pq;
-        for (const auto &c: costs) {
-            res += c[0];
-            pq.push(c[0] - c[1]);
-            
-            if (pq.size() > n / 2) {
-                res -= pq.top();
-                pq.pop();
-            }
-        }
+        sort(begin(costs), end(costs), [&](auto& a, auto& b){
+            return (a[0] - a[1]) < (b[0] - b[1]);
+        });
 
+        for (int i = 0; i < n; i++) {
+            res += costs[i][(i >= n / 2)];
+        }
         return res;
     }
 };
